51b4de25a9e5a3066606385239b0191073f4d027
Andrey Koval <koval.andrey@gmail.com>
2015-02-23 19:54:24 -0800

    automate collection

--- studies/collect_studies.R
+++ studies/collect_studies.R
@@ -1,5 +1,9 @@
 
-study <- c("octo")
+study <- c("eas", "esla", "habc", "hrs", "ilse", "lasa", "nas", "nshd", "nuage", "obas",   "octo", "radc", "satsa" )
+
+# execute collect_STUDY.R code only for studies, which folders contain "collected_params.csv" file
+# from each .cvs file, draw only the rows for which $model_number is not NA
+# or lalternatively don't run studies which $model_number has any NA
 
 for (i in study){
 
