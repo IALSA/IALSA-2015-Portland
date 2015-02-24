d9f17608049cf05967fbd46ebb9832ec1a3c9957
Andrey Koval <koval.andrey@gmail.com>
2015-02-23 20:59:54 -0800

    standardized collect scripts

--- studies/satsa/collect_satsa.R
+++ studies/satsa/collect_satsa.R
@@ -9,6 +9,10 @@
 options(width=160)
 rm(list=ls())
 
+#####################################
+## @knitr load_sources
+source("./synthesis/validate.R") #Loads some functions to validate things like filenames.
+
 library(MplusAutomation)
 pathDir <- getwd() # establish home directory
 studyName <- "satsa"
@@ -47,9 +51,12 @@ results <- data.frame(matrix(NA, ncol=length(dto.vars), nrow=nmodels))
 names(results) <-  dto.vars
 
 for(i in seq_along(mpar)){
-#  i <-5   # for testing and debugging
+  #  i <-5   # for testing and debugging
   mplus_output <- scan(file=file.path(pathStudy, msum$Filename[i]), what='character', sep='\n')
   mplus_model <- strsplit(msum$Filename[i], split="_|.out") # split file name to extract model specs
+
+  #   validate_filename_output(msum$Filename[i]) #Look at the basics of the filename, to see if it matches the correct conventions.
+
 
   mplus_model[[1]][[2]] # returns group identifier: male, female
   mplus_model[[1]][[3]] # returns covariates identifier: empty, age, full
@@ -57,25 +64,25 @@ for(i in seq_along(mpar)){
   mplus_model[[1]][[5]] # returns cognitive outcome: noCOg, knowledge, reasoning, speed, visuospatial, executive, mental
 
 
-## Populate with header info
+  ## Populate with header info
   results[i,c("model_number", 'subgroup',  'model_type', 'physical_outcome', 'cognitive_outcome')] <- mplus_model[[1]][1:5] # extract model specs from output filename
   results[i,"version"] <- "0.1" #msum[i,"Mplus.version"]
   results[i,"active"] <- NA
   results[i, c('date', 'time')] <- strsplit(mplus_output[3], '  ')[[1]]
-results[i,"study_name"] <- studyName
+  results[i,"study_name"] <- studyName
   # Extract the file path, which exists after 'File = ', but before the semicolon.
   results[i, "data_file"] <- gsub(pattern="DATA:  File = (.+);", replacement="\\1", mplus_output[6], perl=T)
 
 
-## Check for model conversion
+  ## Check for model conversion
   has_converged <- (grep("THE MODEL ESTIMATION TERMINATED NORMALLY", mplus_output) >= 1)
   results[i, 'converged'] <- has_converged
   if(has_converged) {
-      ## obtain model for current loop
-      model <- mpar[[i]]$unstandardized; model
+    ## obtain model for current loop
+    model <- mpar[[i]]$unstandardized; model
 
-#### DTO: Covariances  ####
-## Look for at least 4 WITH statements - otherwise fall back to 'Means' and 'Variances' (Baseline Models)
+    #### DTO: Covariances  ####
+    ## Look for at least 4 WITH statements - otherwise fall back to 'Means' and 'Variances' (Baseline Models)
     modtype <- (length(grep("WITH", model$paramHeader))>=4)
     modtype
     if(modtype) { # if modtype==1 we have WITH statements
@@ -109,13 +116,13 @@ results[i,"study_name"] <- studyName
       rc <- x[-grep("I|S", x$param),]
       ## Check whether only one cov has been estimated
       if(length(unique(rc$est))==1) {
-          results[i, c("cov_residual", "p_cov_res")] <- rc[1,c('est', 'pval')]
+        results[i, c("cov_residual", "p_cov_res")] <- rc[1,c('est', 'pval')]
       } else {
         results[i, 'notes'] <- paste(results[i, 'notes'], "_Heterogeneous Res Covs")
       }
     }
 
-#### DTO: Variances   ####
+    #### DTO: Variances   ####
 
     ## Subset model
     vrs <- model[grep("Variances", model$paramHeader),]
@@ -138,28 +145,38 @@ results[i,"study_name"] <- studyName
     if(length(resP[,1])==1) {results[i, c("var_residual_physical", "se_residual_physical")] <- resP}
     ## Test of unconstrained variances: needs development
     #else {
-     #    results[i,'notes'] <- paste(results[i,'notes'], 'Phys ResCov unconstrained', sep='_')}
-     resC <- unique(vrs[grep("^C", vrs$param), c('est', 'se')] )
-     ## Write residual covariance and add warning if ResCov unconstrained
-     if(length(resC[, 1])==1) {results[i, c("var_residual_cog", "se_residual_cog")] <- resC}
+    #    results[i,'notes'] <- paste(results[i,'notes'], 'Phys ResCov unconstrained', sep='_')}
+    resC <- unique(vrs[grep("^C", vrs$param), c('est', 'se')] )
+    ## Write residual covariance and add warning if ResCov unconstrained
+    if(length(resC[, 1])==1) {results[i, c("var_residual_cog", "se_residual_cog")] <- resC}
     ## Test of unconstrained variances: needs development
     #else {
-     #    results[i,'notes'] <- paste(results[i,'notes'], 'Cog ResCov unconstrained', sep='_')}
-}
+    #    results[i,'notes'] <- paste(results[i,'notes'], 'Cog ResCov unconstrained', sep='_')}
+  }
 
-#### DTO: performance  ####
+  #### DTO: performance  ####
 
-    results[i, 'subject_count'] <- msum[i, 'Observations']
-    results[i, 'wave_count'] <- 'to_do'
-    results[i, 'parameter_count'] <- msum[i, 'Parameters']
-    results[i, 'output_file'] <- msum[i, 'Filename']
-    results[i, 'software'] <- mplus_output[1]
-    results[i, 'model_description'] <- '??'
-    results[i, c('LL')] <-  msum[i,c('LL')]
-    results[i, c('aic')] <-  msum[i,c('AIC')]
-    results[i, c('bic')] <-  msum[i,c('BIC')]
-    results[i, c('adj_bic')] <-  msum[i,c('aBIC')]
-    results[i, c('aaic')] <-  msum[i,c('AICC')]
+  #Progressively building regex to match the model line:
+  # MODEL:  ip sp qp \| p1-p5 AT time1-time(\d{1,3});
+  # ^  MODEL:  .+ \| p1-p5 AT time1-time(\d{1,3});\s*$
+  # ^\s{0,}MODEL:.{1,}\|.{1,} AT .{1,}time(\d{1,3});\s{0,}$
+
+  pattern_time_max <- "^\\s{0,}MODEL:.{1,}\\|.{1,} AT .{1,}time(\\d{1,3});\\s{0,}$"
+  model_line <- grep(pattern_time_max, x=mplus_output, perl=T, value=T)
+  str_max_time <- gsub(pattern_time_max, "\\1", model_line, perl=TRUE);
+  wave_count <- as.integer(str_max_time)
+
+  results[i, 'subject_count'] <- msum[i, 'Observations']
+  results[i, 'wave_count'] <- wave_count
+  results[i, 'parameter_count'] <- msum[i, 'Parameters']
+  results[i, 'output_file'] <- msum[i, 'Filename']
+  results[i, 'software'] <- mplus_output[1]
+  results[i, 'model_description'] <- '??'
+  results[i, c('LL')] <-  msum[i,c('LL')]
+  results[i, c('aic')] <-  msum[i,c('AIC')]
+  results[i, c('bic')] <-  msum[i,c('BIC')]
+  results[i, c('adj_bic')] <-  msum[i,c('aBIC')]
+  results[i, c('aaic')] <-  msum[i,c('AICC')]
 }
 
 results <- dplyr::arrange(results, physical_outcome,  )
@@ -169,5 +186,4 @@ results <- dplyr::arrange(results, physical_outcome,  )
 View(results)
 ## Write populated dto for further use
 write.csv(results, file=file.path(pathStudy,'collected_params.csv'), row.names = F)
-
 
