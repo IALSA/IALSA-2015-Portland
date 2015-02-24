d6e3af87ed12dbd0406212f94dbeefbc7fe12b87
Will Beasley <wibeasley@hotmail.com>
2015-02-23 18:12:07 -0600

    Prettify tables with Andrey on the phone.

--- synthesis/model_synthesis.R
+++ synthesis/model_synthesis.R
@@ -13,7 +13,8 @@ library(scales) #For formating values in graphs
 library(RColorBrewer)
 # library(reshape2) #For converting wide to long
 library(ggplot2) #For graphing
-# library(mgcv, quietly=TRUE) #For the Generalized Additive Model that smooths the longitudinal graphs.
+library(testit, quietly=TRUE) #For asserts
+
 
 #####################################
 ## @knitr declare_globals
@@ -51,7 +52,29 @@ rm(dto_path, study_name, dto)
 # http://stackoverflow.com/questions/2851327/converting-a-list-of-data-frames-into-one-data-frame-in-r
 ds <- plyr::ldply(dtos, data.frame)
 
+is_univariate <- grepl(pattern="^u\\d$", x=ds$model_number)
+is_bivariate <- grepl(pattern="^b\\d$", x=ds$model_number)
+testit::assert("The model number should match the univariate or bivariate pattern.", is_univariate | is_bivariate)
+ds$outcome_count <- ifelse(is_univariate, 1L, 2L)
 
+ds_pretty <- ds
+# ds$var_int_cog <- round(ds$var_int_cog, 2)
+ds_pretty$var_int_cog <- sprintf("%.2f", ds_pretty$var_int_cog) #Force it to have one decimal, even if it's a zero.
+ds_pretty$var_int_cog <- ifelse(ds_pretty$var_int_cog=="NA", "--", ds_pretty$var_int_cog)
+
+desired_columns_univariate <- c("model_number", "study_name", "subgroup", "model_type", "physical_outcome", "var_int_cog")
+desired_columns_bivariate <- c("model_number", "study_name", "subgroup", "model_type", "physical_outcome")
+
+ds_univariate_pretty <- ds_pretty[ds_pretty$outcome_count==1L, desired_columns_univariate]
+ds_bivariate_pretty <- ds_pretty[ds_pretty$outcome_count==2L, desired_columns_bivariate]
+
+ds_univariate_pretty <- plyr::rename(ds_univariate_pretty, replace=c(
+  "model_number" = "model",
+  "study_name" = "study",
+  "subgroup" = "subgroup",
+  "model_type" = "model<br/>type",
+  "physical_outcome" = "outcome<br/>physical"
+))
 #####################################
 ## @knitr calculate_forest
 #TODO: Andrey, calcalulate the graph coordinates, using Philipe's equations that start with
@@ -61,13 +84,26 @@ ds <- plyr::ldply(dtos, data.frame)
 #####################################
 ## @knitr study_tables
 
+
 for( i in seq_along(dtos) ) {
   dto <- dtos[[i]]
   study_name <- study_names[i]
-  cat("")
+
+  d_univariate_study <- ds_univariate_pretty[ds_univariate_pretty$study==study_name, ]
+  d_bivariate_study <- ds_bivariate_pretty[ds_bivariate_pretty$study==study_name, ]
+
+  cat("") #Force a new line
   cat(paste0("## ", study_name))
-  cat("")
-  print(knitr::kable(dto))
+
+  cat("\n") #Force a new line
+  # cat("#### Univariate results")
+  # cat("\n") #Force a new line
+  print(knitr::kable(d_univariate_study, caption="Univariate Results"))
+
+  cat("\n") #Force a new line
+  # cat("#### Bivariate results")
+  # cat("\n") #Force a new line
+  print(knitr::kable(d_bivariate_study, caption="Bivariate Results"))
   cat("\n")
 }
 
