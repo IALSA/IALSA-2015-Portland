c7a3f577ddb21ab45f8404618102741699a9300c
Andrey Koval <koval.andrey@gmail.com>
2015-04-23 13:27:26 -0700

    update

--- thesaurus.r
+++ thesaurus.r
@@ -1,17 +1,20 @@
 ## The script demonstrates and explains the use of main functions in the this repository
 
+getwd()
+pathFile <- "./studies/b1_female_ae_muscle_fluency_grip_bostonnamingTest.out"
+
 
 # strsplit()
 # Split the Elements of a Character Vector
-a <- "b1_female_ae_muscle_fluency_grip_bostonnaming.out"
-b <- strsplit(a,"_")
+
+b <- strsplit(pathFile,"_")
 b
-c <- strsplit(a,"_|.out")
+c <- strsplit(pathFile,"_|.out")
 c
 
 # scan()
 # reads data into a vector or a list from console or file
-a <- scan("C:/Users/andkov/Documents/GitHub/IALSA-2015-Portland/studies/eas/b1_female_aeh_pulmonary_memory_pek_digitspan.out", what='character', sep='\n')
+a <- scan(pathFile, what='character', sep='\n')
 a
 
 
@@ -20,4 +23,13 @@ a
 a <- strsplit("Copyright (c) 1998-2014 Muthen & Muthen",split=" ")
 a[[1]]=="Muthen"
 b <- which(a[[1]]=="Muthen")
+b
+
+# grep ()
+# Pattern Matching and Replacement
+#  return each line of the file as character string
+a <- scan(pathFile, what='character', sep='\n')
+str(a) # character vector with 557 element
+# return the number of the element that contains the patten
+b <- grep("CONFIDENCE INTERVALS OF MODEL", a)
 b
