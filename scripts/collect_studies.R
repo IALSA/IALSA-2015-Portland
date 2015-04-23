7fac8cbc0b6c6ffbba854149dd15eb96b8ee61d1
Andrey Koval <koval.andrey@gmail.com>
2015-04-23 15:26:37 -0700

    update interface

--- scripts/collect_studies.R
+++ scripts/collect_studies.R
@@ -7,5 +7,15 @@ library(MplusAutomation)
 ## @knitr setPaths
 
 pathDir <- getwd() # establish home directory
-pathStudy <- file.path(pathDir,"studies") 
-list.files(pathStudy)
+pathStudies <- file.path(pathDir,"studies") 
+list.files(pathStudies) # inspect participating studies
+
+## @knitr setGlobals
+studies <- c("eas", "elsa")
+out_list_all <- list.files(pathStudies, full.names=T, recursive=T, pattern="out$")
+
+
+
+findConflicts("eas")
+findConflicts("habc")
+
