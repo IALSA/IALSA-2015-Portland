5706c770b8693d6b84b3fe943fa710315a7a3a2e
Andrey Koval <koval.andrey@gmail.com>
2015-06-07 20:01:53 -0700

    tinkering stable version of counts report

--- scripts/2_individual_study_report.R
+++ scripts/2_individual_study_report.R
@@ -21,19 +21,16 @@ library(dplyr)
 dsb <- readRDS('./data/shared/ds1a.rds')
 keepvar <- c("model_number","study_name", "model_type","physical_construct","cognitive_construct","physical_measure","cognitive_measure", "output_file")
 
-# ds <- dsb[ , keepvar]
+ds <- dsb[ , keepvar]
 
 ## @knitr dummy
-# key columns/variables
-keycol <- c("model_number"="Alpha numeric ","study_name","converged", )
-keycolF <- factor(dsb$keycol, values
 
 unique(dsb$study_name)
 
 names(dsb)
 
 ## @knitr number_studies
-length(dsb$study_name)
+cat(paste0("**",length(dsb$study_name),"**"))
 
 # @knitr missing_studies
 pathDir <- getwd() # establish home directory
@@ -42,7 +39,7 @@ misslong <- list.dirs(pathStudies, recursive = F)
 missing <- basename(misslong)
 present <- unique(dsb$study_name)
 miss <- missing[!(missing %in% present)]
-miss
+cat(miss)
 
 
 ## @knitr freq_studies
@@ -50,39 +47,109 @@ t1<-table(dsb$study_name)
 t1[t1==0] <- "."
 t1
 
-## @knitr freq_phys_studies
-t2 <- table(dsb$physical_construct,dsb$study_name)
-t2[t2==0] <- "."
-t2
 
-## @knitr freq_cog_studies
-t3 <- table(dsb$cognitive_construct,dsb$study_name)
-t3[t3==0] <- "."
-t3
+## @knitr dummy
+## PHYSICAL DOMAIN
 
-## @knitr freq_cog_phys
+## @knitr freq_phys_constructs_studies
+t2a <- table(dsb$physical_construct,dsb$study_name)
+t2a[t2a==0] <- "."
+t2a
+
+## @knitr freq_phys_measures_studies
+t2b <- table(dsb$physical_measure,dsb$study_name)
+t2b[t2b==0] <- "."
+t2b
+
+## @knitr freq_phys_measures_phys_construct
+t2c <- table(dsb$physical_measure,dsb$physical_construct)
+t2c[t2c==0] <- "."
+t2c
+
+## @knitr freq_phys_measures_phys_construct_studies
+t2d <- table(dsb$study, dsb$physical_measure,dsb$physical_construct)
+t2d[t2d==0] <- "."
+ftable(t2d)
+
+
+## @knitr dummy
+## COGNITIVE DOMAIN
+
+## @knitr freq_cog_constructs_studies
+t3a <- table(dsb$cognitive_construct,dsb$study_name)
+t3a[t3a==0] <- "."
+t3a
+
+## @knitr freq_cog_measures_studies
+t3b <- table(dsb$cognitive_measure,dsb$study_name)
+t3b[t3b==0] <- "."
+t3b
+
+## @knitr freq_cog_measures_cog_construct
+t3c <- table(dsb$cognitive_measure,dsb$cognitive_construct)
+t3c[t3c==0] <- "."
+t3c
+
+## @knitr freq_cog_measures_cog_construct_studies
+t3d <- table(dsb$study, dsb$cognitive_measure,dsb$cognitive_construct)
+t3d[t3d==0] <- "."
+ftable(t3d)
+
+
+
+
+## @knitr CROSSING DOMAINS
+
+## @knitr freq_cog_construct_phys_construct
 t4 <- table(dsb$cognitive_construct,dsb$physical_construct)
 t4[t4==0] <- "."
 t4
 
-## @knitr freq_cog_phys_studies
 
-t5 <- table(dsb$study,dsb$cognitive_construct,dsb$physical_construct)
+## @knitr freq_cog_measure_phys_measure
+t5 <- table(dsb$cognitive_measure,dsb$physical_measure)
 t5[t5==0] <- "."
-ftable(t5)
+t5
 
-
-## @knitr freq_phys_measure
-t6 <- table(dsb$study, dsb$physical_measure, dsb$physical_construct)
+## @knitr freq_cog_measure_phys_measure_studies_allnames
+t6 <- table(dsb$study, dsb$cognitive_measure,dsb$physical_measure)
 t6[t6==0] <- "."
 ftable(t6)
 
 
-## @knitr freq_phys_measure
 
+
+
+## @knitr freq_cog_measure_phys_measure_studies
+  for( study in unique(dsb$study_name)){
+    ds <- dsb[dsb$study_name==study,]
+    t7 <- table( ds$cognitive_measure, ds$physical_measure)
+    t7[t7==0] <- "."
+
+  cat("") #Force a new line
+  cat(paste0("### ", study))
+  cat("\n")
+  cat(paste0("Number of models in the **",study,"** study with the following combination of physical measures (columns) and cognitive measure (rows) "))
+  cat("\n")
+  cat("```")
+  cat("\n")
+    if( !is.na(t7[1])){
+    # print(ftable(t7))
+      print(t7)
+    } else {
+    cat(paste0("Error in file naming: specific measurement was not specified for physical construct **", cs, "**.*\n\n"))
+    }
+    cat("```")
+    cat("\n\n")
+  }
+
+
+
+
+## @knitr freq_phys_measure
   for( cs in unique(dsb$physical_construct)){
     ds <- dsb[dsb$physical_construct==cs,]
-    t7 <- table( ds$physical_measure, ds$study_name)
+    t7 <- table( ds$physical_measure, ds$study_name) # physical_measure
     t7[t7==0] <- "."
 
   cat("") #Force a new line
@@ -102,11 +169,11 @@ ftable(t6)
     cat("\n\n")
   }
 
-## @knitr freq_cog_measure
 
+## @knitr freq_cog_measure
   for( cs in unique(dsb$cognitive_construct)){
     ds <- dsb[dsb$cognitive_construct==cs,]
-    t7 <- table( ds$cognitive_measure, ds$study_name)
+    t7 <- table( ds$cognitive_measure, ds$study_name) # cognitive_measure
     t7[t7==0] <- "."
 
   cat("") #Force a new line
@@ -181,31 +248,4 @@ for( i in seq_along(present) ) {
 
 # }
 
-#
-# for( i in seq_along(present) ) {
-#
-#   study_name <- present[i]
-#
-#
-#   d_bivariate_study <- ds_bivariate_pretty[ds_bivariate_pretty$study==study_name, ]
-#   # d_bivariate_study <- d_bivariate_study[ , -(1:2)]
-#   cat("") #Force a new line
-#   cat(paste0("## ", study_name))
-#   cat("\n") #Force a new line
-#
-#   for(gender in unique(d_bivariate_study$subgroup)){
-#   cat("") #Force a new line
-#   cat(paste0("### ", gender))
-#   cat("\n") #Force a new line
-#   d <- d_bivariate_study[d_bivariate_study$subgroup==gender, ]
-#
-#     if( nrow(d_bivariate_study) > 0L) {
-#       print(knitr::kable(d[ , -(1:2)], caption="Bivariate Results", row.names= F))
-#     } else {
-#       cat("*No bivariate models were supplied from the study.*\n\n")
-#     }
-#     cat("\n")
-#   }
-#
-# }
 
