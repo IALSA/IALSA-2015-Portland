e9ec603f9ebb7d0ad11c8b18cb389621839af150
Andrey Koval <koval.andrey@gmail.com>
2015-06-11 12:39:44 -0700

    basic server restored

--- shiny/basic/server.R
+++ shiny/basic/server.R
@@ -1,24 +1,7 @@
-## pivot tables in Shiny
-
-# Article on pivot tables in R - http://www.magesblog.com/2015/03/pivot-tables-with-r.html
-
-# rpivotTable Documentation - https://github.com/smartinsightsfromdata/rpivotTable
-
-# GitHub Example - https://github.com/smartinsightsfromdata/rpivotTable/blob/master/inst/examples/canElections-shiny.R
-
 library(shiny)
-library(rpivotTable)
 
-if(basename(getwd())=="pivotTable"){
 dsb <- readRDS('../../data/shared/ds1a.rds')
-}else{
-dsb <- readRDS('./data/shared/ds1a.rds')
-}
-
-
-
-ds <- dsb[ , c( "physical_construct","physical_measure","cognitive_measure","cognitive_construct",
-                "study_name", "model_type","subgroup")]
+ds <- dsb[ , c("study_name", "model_type","physical_construct","cognitive_construct","physical_measure","cognitive_measure","sd_int","p_cov_int","sd_slope","p_cov_slope")]
 head(ds)
 
 
@@ -28,9 +11,38 @@ unique(ds$physical_construct)
 
 # Define server logic required to summarize and view the selected
 # study
-shinyServer(function(input, output) {
- output$pivot <- renderRpivotTable({
-   rpivotTable(data = ds)
- })
+shinyServer(function(input, output,clientData,session) {
 
+# Develope handler for when study variables is empty # use handlers / if()
+  observe({
+    # browser()
+# ds_study <- ds[ds$study_name==input$study, ]
+study_variables <- sort(unique(ds_study()$physical_construct))
+updateSelectInput(session, "physical",
+                     choices = study_variables,
+                     selected=study_variables[1])
+})
+
+ds_study <- reactive({
+  ds[ds$study_name==input$study, ]
+  })
+
+  # Generate frequency count for the study
+  output$freqs <- renderPrint({
+
+    table(ds_study()$physical_construct,ds_study()$cognitive_construct)
+#
+  })
+#
+#   # Show the first "n" observations
+  output$view <- renderDataTable({
+    ds_study_physical <- ds_study()[ds_study()$physical_construct==input$physical, ]
+    head(ds_study_physical, n=input$obs)
+  },
+  options = list(
+    searching = F,
+    paging = FALSE,
+    sort = FALSE,
+    language= list(emptyTable="--<em>This table has no entries</em>--"))
+  )
 })
