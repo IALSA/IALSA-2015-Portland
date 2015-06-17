32b93ce6a901ccc82d78d71fdecc527c9de12a85
Andrey Koval <koval.andrey@gmail.com>
2015-06-17 10:33:26 -0700

    init table 2

--- reports/table_2/table_2.R
+++ reports/table_2/table_2.R
@@ -27,8 +27,56 @@ source("./shiny/dashboard/scripts/multiplot_function.R")
 
 ## @knitr tweak_data
 ## trim to make more managable
+keepvar <- c( "physical_construct","physical_measure","cognitive_measure","cognitive_construct","study_name", "model_type","subgroup", "converged", "output_file", "corr_int", "corr_slope", "corr_residual", "ciu_corr_int", "cil_corr_int", "ciu_corr_slope", "cil_corr_slope", "ciu_corr_residual", "cil_corr_residual", "p_cov_int", "p_cov_slope", "p_cov_res")
+
 # keepvar <- c("model_number","study_name","subgroup", "model_type","physical_construct","cognitive_construct","physical_measure","cognitive_measure", "output_file", "converged")
-# ds <- dsb[ , keepvar]
+ds <- dsb[ , keepvar]
+
+ds$display_int <- paste0(
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_int, 2)), " \n (*",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$cil_corr_int,2)), ",",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_int,2)), "*)"
+)
+
+
+ds$display_slope <- paste0(
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_slope, 2)), " \n (*",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$cil_corr_slope,2)), ",",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_slope,2)), "*)")
+
+ds$display_residual <- paste0(
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_residual, 2)), " \n (*",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$cil_corr_residual,2)), ",",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_residual,2)), "*)")
+
+head(ds)
+
+## rename for pretty display
+ds <- ds[ , c( "study_name", "subgroup","model_type","physical_construct","physical_measure","cognitive_measure","cognitive_construct",  "corr_int", "corr_slope", "corr_residual", "display_int","display_slope", "display_residual", "p_cov_int", "p_cov_slope", "p_cov_res","converged", "output_file") ]
+
+head(ds)
+
+library(dplyr)
+# ds <- ds %>%
+#   dplyr::rename_("Phys.Domain" = "physical_construct") %>%
+#   dplyr::rename_("Phys.Measure" = "physical_measure") %>%
+#   dplyr::rename_("Cog.Domain" = "cognitive_construct") %>%
+#   dplyr::rename_("Cog.Measure" = "cognitive_measure") %>%
+#   dplyr::rename_("Study" = "study_name") %>%
+#   dplyr::rename_("Covariates" = "model_type") %>%
+#   dplyr::rename_("Sex" = "subgroup") %>%
+#   dplyr::rename_("Corr.Intersepts" = "corr_int") %>%
+#   dplyr::rename_("Corr.Slopes" = "corr_slope") %>%
+#   dplyr::rename_("Corr.Residuals" = "corr_residual") %>%
+#   dplyr::rename_("Intercept" = "display_int") %>%
+#   dplyr::rename_("Slope" = "display_slope") %>%
+#   dplyr::rename_("Residual" = "display_residual") %>%
+#   dplyr::rename_("p(i)" = "p_cov_int") %>%
+#   dplyr::rename_("p(s)" = "p_cov_slope") %>%
+#   dplyr::rename_("p(r)" = "p_cov_res")
+# head(ds)
+
+
 # dplyr::tbl_df(ds)
 
 
@@ -46,7 +94,7 @@ theme1 <- ggplot2::theme_bw(base_size=baseSize) +
 #   ggplot2::theme(panel.grid.major.y = element_blank())
 #   ggplot2::theme(panel.grid.minor.x = element_blank())
 #   ggplot2::theme(panel.grid.minor.y = element_blank())
- ggplot2::theme(strip.text.x = element_text(angle = 0, size=baseSize-3, color="black"))
+ # ggplot2::theme(strip.text.x = element_text(angle = 0, size=baseSize-3, color="black"))
 
 ## @knitr declare_globals
 ## Define color palette and display labels
@@ -59,47 +107,81 @@ theme1 <- ggplot2::theme_bw(base_size=baseSize) +
                      "model_type"="Covariates",
                      "subgroup"="Subgroup")
 
-## @knitr define_graph_functions
+
+  ## @knitr
 
 
+  ## @knitr define_graph_functions
+# table_2 <- function(ds, x_name, vfacet){
+#   d <- ds %>%
+#   dplyr::filter( !(model_type %in% c("0","aeplus", "full"))) %>%
+#   dplyr::count_(c("cognitive_measure", x_name))
+#   d$dummy <- factor("dummy")
+#   d$cog_meas <- stringr::str_sub(d$cognitive_measure,1,3)
 
-
-table_2 <- function(ds, x_name){
+# x_name <- "model_type"
+# vfacet <- "cognitive_construct"
+#
+# table_2 <- function(ds, x_name, vfacet){
   d <- ds %>%
-  dplyr::count_(c("cognitive_measure", x_name))
-  d$dummy <- factor("dummy")
-  d$cog_meas <- stringr::str_sub(d$cognitive_measure,1,3)
+  dplyr::filter(study_name == "octo", physical_measure == "grip") %>%
+  dplyr::filter(!cognitive_construct %in% c("Univar")) %>%
+  dplyr::filter_( !("model_type" %in% c("0","aeplus", "full"))) %>%
+  dplyr::count_(c("model_type", "cognitive_construct","cognitive_measure","corr_int" , "display_int", "display_slope", "display_residual", "p_cov_int", "p_cov_slope", 'p_cov_res', "subgroup")) %>%
+  tidyr::gather("parameter", "pvalue", 8:10
+                # seq_along(names(d) %in% c( "p_cov_int", "p_cov_slope", 'p_cov_res'))
+                )
+  d$parameter <- stringr::str_replace(d$parameter,"p_cov_", "")
+
+  d$unsign <- d$pvalue > .10
+  d$sign10 <- d$pvalue <= .10
+  d$sign05 <- d$pvalue <= .5
+  d$sign01 <- d$pvalue <= .01
+  d$sign001 <- d$pvalue <= .001
+
+  d$sign <- ifelse(d$pvalue >.10, ">.10",
+              ifelse(d$pvalue <= .10 & d$pvalue > .05, ".10",
+                ifelse(d$pvalue <= .05 & d$pvalue > .01, ".05",
+                  ifelse(d$pvalue <= .01 & d$pvalue > .001, ".01",
+                    ifelse(d$pvalue <= .001 & d$pvalue > .0, ".001", NA)))))
+
+  head(as.data.frame(d))
+
+
+
+#   head(as.data.frame(d))
+#   str(d)
+# head(d)
 
   # d
-  g <- ggplot2::ggplot(d, aes_string(x="dummy", y="cognitive_measure", label="n", fill="n"))
+  g <- ggplot2::ggplot(d, aes_string(x="model_type", y="cognitive_measure", label="pvalue", fill="sign"))
   g <- g + geom_tile()
-  g <- g + geom_text(size = baseSize-8)
-  g <- g + facet_grid(reformulate(x_name))
+  g <- g + geom_text(size = baseSize-5)
+  g <- g + facet_grid(subgroup ~ parameter)
   # g <- g + coord_flip()
   g <- g + scale_y_discrete(name = "Cognitive measures", limits=rev(unique(d$cognitive_measure)))
-  # g <- g + scale_fill_discrete(name = "Domains")
-  g <- g + scale_fill_gradient(low="white", high=x_name_colors[x_name], na.value = "white")
-  g <- g + labs(title=x_name_labels[x_name])
+  g <- g + scale_fill_discrete(name = "Domains")
+  # g <- g + scale_fill_gradient(low="white", high=x_name_colors["subgroup"], na.value = "grey")
+  # g <- g + labs(title=x_name_labels["subgroup"])
   g <- g + theme1
   g <- g + theme(axis.text.y =  element_text(size=baseSize-1),
-                 axis.text.x =  element_blank(),
-                 axis.title.x = element_blank(),
-                 axis.title.y = element_blank(),
+                 # axis.text.x =  element_blank(),
+                 # axis.title.x = element_blank(),
+                 # axis.title.y = element_blank(),
                  legend.title = element_text(),
                  panel.grid.major.x = element_blank(),
-                 # legend.text =  element_text(),
-                 legend.position="none")
-  # g <- g + theme(strip.text.x = element_text(angle = 0, size=baseSize-3, color="black"))
+                 legend.text =  element_text(),
+                 legend.position="right")
+  g <- g + theme(strip.text.x = element_text(angle = 0, size=baseSize-3, color="black"))
   g
 
-  return(g)
-}
+#   return(g)
+# }
 
 
 ## @knitr combine_map_and_cross
 
-b <- table_2(dsb,"physical_measure")
-
+table_2(dsb,"model_type", "cognitive_construct")
 
 
 
