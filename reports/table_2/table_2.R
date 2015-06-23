fd597e3cedc972b00b607bbfd1192d3cd5a672be
Andrey Koval <koval.andrey@gmail.com>
2015-06-23 11:58:51 -0700

    table 2, flexible function

--- reports/table_2/table_2.R
+++ reports/table_2/table_2.R
@@ -1,3 +1,5 @@
+rm(list=ls(all=TRUE))
+cat("\f")
 ## @knitr dummy
 ############## Developing the tile graph #######################
 
@@ -27,47 +29,47 @@ source("./shiny/dashboard/scripts/multiplot_function.R")
 
 ## @knitr tweak_data
 ## trim to make more managable
-keepvar <- c( "physical_construct","physical_measure","cognitive_measure","cognitive_construct","study_name", "model_type","subgroup", "converged", "output_file", "corr_int", "corr_slope", "corr_residual", "ciu_corr_int", "cil_corr_int", "ciu_corr_slope", "cil_corr_slope", "ciu_corr_residual", "cil_corr_residual", "p_cov_int", "p_cov_slope", "p_cov_res")
+keepvar <- c("study_name", "subgroup", "model_type","physical_construct","physical_measure", "cognitive_construct","cognitive_measure", "converged", "output_file", "corr_int", "corr_slope", "corr_residual", "ciu_corr_int", "cil_corr_int", "ciu_corr_slope", "cil_corr_slope", "ciu_corr_residual", "cil_corr_residual", "p_cov_int", "p_cov_slope", "p_cov_res")
 
 # keepvar <- c("model_number","study_name","subgroup", "model_type","physical_construct","cognitive_construct","physical_measure","cognitive_measure", "output_file", "converged")
 ds <- dsb[ , keepvar]
 
 ds$display_int <- paste0(
-  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_int, 2)), " \n (*",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_int, 2)), " \n (",
   gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$cil_corr_int,2)), ",",
-  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_int,2)), "*)"
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_int,2)), ")"
 )
 
-
 ds$display_slope <- paste0(
-  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_slope, 2)), " \n (*",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_slope, 2)), " \n (",
   gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$cil_corr_slope,2)), ",",
-  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_slope,2)), "*)")
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_slope,2)), ")")
 
 ds$display_residual <- paste0(
-  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_residual, 2)), " \n (*",
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$corr_residual, 2)), " \n (",
   gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$cil_corr_residual,2)), ",",
-  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_residual,2)), "*)")
+  gsub("^(\\[+-])?(0)?(\\.\\d+)$", "\\1\\3", round(ds$ciu_corr_residual,2)), ")")
+
 
 head(ds)
 
 ## rename for pretty display
-ds <- ds[ , c( "study_name", "subgroup","model_type","physical_construct","physical_measure","cognitive_measure","cognitive_construct",  "corr_int", "corr_slope", "corr_residual", "display_int","display_slope", "display_residual", "p_cov_int", "p_cov_slope", "p_cov_res","converged", "output_file") ]
-
-head(ds)
+# ds <- ds[ , c( "study_name", "subgroup","model_type","physical_construct","physical_measure","cognitive_measure","cognitive_construct",  "corr_int", "corr_slope", "corr_residual", "display_int","display_slope", "display_residual", "p_cov_int", "p_cov_slope", "p_cov_res","converged", "output_file") ]
+#
+# head(ds)
 
 library(dplyr)
 # ds <- ds %>%
-#   dplyr::rename_("Phys.Domain" = "physical_construct") %>%
-#   dplyr::rename_("Phys.Measure" = "physical_measure") %>%
-#   dplyr::rename_("Cog.Domain" = "cognitive_construct") %>%
-#   dplyr::rename_("Cog.Measure" = "cognitive_measure") %>%
+#   dplyr::rename_("PhysDomain" = "physical_construct") %>%
+#   dplyr::rename_("PhysMeasure" = "physical_measure") %>%
+#   dplyr::rename_("CogDomain" = "cognitive_construct") %>%
+#   dplyr::rename_("CogMeasure" = "cognitive_measure") %>%
 #   dplyr::rename_("Study" = "study_name") %>%
 #   dplyr::rename_("Covariates" = "model_type") %>%
 #   dplyr::rename_("Sex" = "subgroup") %>%
-#   dplyr::rename_("Corr.Intersepts" = "corr_int") %>%
-#   dplyr::rename_("Corr.Slopes" = "corr_slope") %>%
-#   dplyr::rename_("Corr.Residuals" = "corr_residual") %>%
+#   dplyr::rename_("CorrIntersepts" = "corr_int") %>%
+#   dplyr::rename_("CorrSlopes" = "corr_slope") %>%
+#   dplyr::rename_("CorrResiduals" = "corr_residual") %>%
 #   dplyr::rename_("Intercept" = "display_int") %>%
 #   dplyr::rename_("Slope" = "display_slope") %>%
 #   dplyr::rename_("Residual" = "display_residual") %>%
@@ -107,43 +109,115 @@ theme1 <- ggplot2::theme_bw(base_size=baseSize) +
                      "model_type"="Covariates",
                      "subgroup"="Subgroup")
 
+# 8th multi-hue at colorbrewer2.org, 5 categories, first 4
+  pvalueColors <- c(">.10" = "white",
+                    "<=.10" = "#feebe2",
+                    "<=.05" = "#fbb4b9",
+                    "<=.01" = "#f768a1",
+                    "<=.001" = "#c51b8a")
 
   ## @knitr
 
 
-  ## @knitr define_graph_functions
-# table_2 <- function(ds, x_name, vfacet){
-#   d <- ds %>%
-#   dplyr::filter( !(model_type %in% c("0","aeplus", "full"))) %>%
-#   dplyr::count_(c("cognitive_measure", x_name))
-#   d$dummy <- factor("dummy")
-#   d$cog_meas <- stringr::str_sub(d$cognitive_measure,1,3)
+# study = "octo"
+# x_name = "parameter"
+# physical_measure = "grip"
+# display_value = "pvalue"
+# cognitive_construct = "memory"
+# x_facet = "model_type"
+# y_facet = "subgroup"
+# covars = unique(ds$model_type)
+# #
 
-# x_name <- "model_type"
-# vfacet <- "cognitive_construct"
+#   # @knitr define_graph_functions
+table_2 <- function(ds
+                    # select
+                    , study = "octo"
+                    , physical_measure = "grip"
+                    , covars = unique(ds$model_type) # model_type
+                    , cognitive_construct = unique(ds$cognitive_construct)
+                    # map
+                    , x_name = "parameter" # x-axis
+                    , display_value = "pvalue" # number that prints
+                    # arrange
+                    , x_facet = "model_type" # grouped horizontally
+                    , y_facet = "subgroup" # grouped vertically
+                    ){
+
+
+
+  d <- ds[ds$study_name %in% study &
+          ds$physical_measure %in% physical_measure &
+          ds$model_type %in% covars &
+          ds$cognitive_construct %in% cognitive_construct , ]
+
+
+  d <- d %>%
+  dplyr::filter(!cognitive_construct %in% c("Univar")) %>% #, !physical_construct %in% c("Univar")) %>%
+  dplyr::count_(c("study_name", "subgroup", "model_type","physical_construct","physical_measure", "cognitive_construct","cognitive_measure","corr_int", "corr_slope", "corr_residual" , "display_int", "display_slope", "display_residual", "p_cov_int", "p_cov_slope", 'p_cov_res')) %>%
+
+# ## one long gather
+#   tidyr::gather_("parameter","value", c("p_cov_int", "p_cov_slope", 'p_cov_res'))
 #
-# table_2 <- function(ds, x_name, vfacet){
-  d <- ds %>%
-  dplyr::filter(study_name == "octo", physical_measure == "grip") %>%
-  dplyr::filter(!cognitive_construct %in% c("Univar")) %>%
-  dplyr::filter_( !("model_type" %in% c("0","aeplus", "full"))) %>%
-  dplyr::count_(c("model_type", "cognitive_construct","cognitive_measure","corr_int" , "display_int", "display_slope", "display_residual", "p_cov_int", "p_cov_slope", 'p_cov_res', "subgroup")) %>%
-  tidyr::gather("parameter", "pvalue", 8:10
-                # seq_along(names(d) %in% c( "p_cov_int", "p_cov_slope", 'p_cov_res'))
-                )
-  d$parameter <- stringr::str_replace(d$parameter,"p_cov_", "")
+#   d$parameter <- stringr::str_replace(d$parameter, "cov_res", "cov_residual")
+#   d$parameter <- stringr::str_replace(d$parameter, "p_cov", "pvalue")
+# for( i in seq_along(d$parameter)){
+#   d[i, "value_type"] <- stringr::str_split(d$parameter, pattern = "_")[[i]][1]
+#   d[i, "parameter"] <- stringr::str_split(d$parameter, pattern = "_")[[i]][2]
+# }
+#   d <- tidyr::spread_(d,"value_type","value")
+#   d$corr_int <- round(as.numeric(d$corr_int),3)
+#   d$corr_slope <- round(as.numeric(d$corr_slope),3)
+#   d$corr_residual <- round(as.numeric(d$corr_residual),3)
+#   d$pvalue <- round(as.numeric(d$pvalue),3)
+#   d$parameter <- factor(d$parameter, levels= c("int", "slope","residual"))
+#   head(as.data.frame(d),20)
+#   str(d)
+
+## test for dublicates
+# attach(d)
+#   d$test <- paste(subgroup,model_type, physical_construct, physical_measure, cognitive_construct, cognitive_measure, sep = "-")
+# detach(d)
+#   nrow(d)
+#   table(d$test)
+#
+# test <- dplyr::count_(d, c("study_name", "subgroup", "model_type","physical_construct","physical_measure", "cognitive_construct","cognitive_measure"))
+
+
+
+
+## three long gather
+  tidyr::gather_("parameter","value", c("corr_int", "corr_slope", "corr_residual" , "display_int", "display_slope", "display_residual", "p_cov_int", "p_cov_slope", 'p_cov_res'))
+  d$parameter <- stringr::str_replace(d$parameter, "cov_res", "cov_residual")
+  d$parameter <- stringr::str_replace(d$parameter, "p_cov", "pvalue")
+for( i in seq_along(d$parameter)){
+  d[i, "value_type"] <- stringr::str_split(d$parameter, pattern = "_")[[i]][1]
+  d[i, "parameter"] <- stringr::str_split(d$parameter, pattern = "_")[[i]][2]
+}
+  d <- tidyr::spread_(d,"value_type","value")
+  d$corr <- round(as.numeric(d$corr),3)
+  d$pvalue <- round(as.numeric(d$pvalue),3)
+  d$parameter <- factor(d$parameter, levels= c("int", "slope","residual"))
+  head(as.data.frame(d),20)
+  str(d)
+
+
+
+
+
+# create dummy for coloring significance
 
   d$unsign <- d$pvalue > .10
-  d$sign10 <- d$pvalue <= .10
-  d$sign05 <- d$pvalue <= .5
-  d$sign01 <- d$pvalue <= .01
+  d$sign10 <- d$pvalue <= .10 & d$pvalue > .05
+  d$sign05 <- d$pvalue <= .05 & d$pvalue > .01
+  d$sign01 <- d$pvalue <= .01 & d$pvalue > .001
   d$sign001 <- d$pvalue <= .001
 
   d$sign <- ifelse(d$pvalue >.10, ">.10",
-              ifelse(d$pvalue <= .10 & d$pvalue > .05, ".10",
-                ifelse(d$pvalue <= .05 & d$pvalue > .01, ".05",
-                  ifelse(d$pvalue <= .01 & d$pvalue > .001, ".01",
-                    ifelse(d$pvalue <= .001 & d$pvalue > .0, ".001", NA)))))
+              ifelse(d$pvalue <= .10 & d$pvalue > .05, "<=.10",
+                ifelse(d$pvalue <= .05 & d$pvalue > .01, "<=.05",
+                  ifelse(d$pvalue <= .01 & d$pvalue > .001, "<=.01",
+                    ifelse(d$pvalue <= .001, "<=.001", NA)))))
 
   head(as.data.frame(d))
 
@@ -153,16 +227,21 @@ theme1 <- ggplot2::theme_bw(base_size=baseSize) +
 #   str(d)
 # head(d)
 
-  # d
-  g <- ggplot2::ggplot(d, aes_string(x="model_type", y="cognitive_measure", label="pvalue", fill="sign"))
+
+
+  # d %>% dplyr::count_( c("study_name", "subgroup", "model_type","physical_construct","physical_measure", "cognitive_construct","cognitive_measure"))
+  # table(d$test)
+  g <- ggplot2::ggplot(d, aes_string(x=x_name, y="cognitive_measure", label=display_value, fill="sign"))
   g <- g + geom_tile()
   g <- g + geom_text(size = baseSize-5)
-  g <- g + facet_grid(subgroup ~ parameter)
-  # g <- g + coord_flip()
+  # g <- g + facet_grid(subgroup ~ model_type)
+  g <- g + facet_grid(as.formula(paste0(y_facet," ~ ", x_facet)))
+
+  # g <- g + scale_x_discrete(labels = c("int"="INT", "slope"="SLP" , "res"="RES"))
   g <- g + scale_y_discrete(name = "Cognitive measures", limits=rev(unique(d$cognitive_measure)))
-  g <- g + scale_fill_discrete(name = "Domains")
+  g <- g + scale_fill_manual(name = "Significance", values = pvalueColors)
   # g <- g + scale_fill_gradient(low="white", high=x_name_colors["subgroup"], na.value = "grey")
-  # g <- g + labs(title=x_name_labels["subgroup"])
+  g <- g + labs(title=paste0(study,"--", physical_measure, "--", display_value))
   g <- g + theme1
   g <- g + theme(axis.text.y =  element_text(size=baseSize-1),
                  # axis.text.x =  element_blank(),
@@ -173,15 +252,27 @@ theme1 <- ggplot2::theme_bw(base_size=baseSize) +
                  legend.text =  element_text(),
                  legend.position="right")
   g <- g + theme(strip.text.x = element_text(angle = 0, size=baseSize-3, color="black"))
-  g
+  # g
 
-#   return(g)
-# }
+  return(g)
+}
 
 
 ## @knitr combine_map_and_cross
 
-table_2(dsb,"model_type", "cognitive_construct")
 
+table_2(ds
+  # select
+  , study = "satsa"
+  , physical_measure = "pek"
+  # , covars = c("a","ae","aeh") # model_type
+  # , cognitive_construct = unique(ds$cognitive_construct)
+  # map
+  , x_name = "parameter" # x-axis
+  , display_value = "corr" # number that prints # corr, display, pvalue
+  # arrange
+  , x_facet = "model_type" # grouped horizontally
+  , y_facet = "subgroup" # grouped vertically
+)
 
 
