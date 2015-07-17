5bf873e5ed5b18714e5268ddb767b847be78fbad
Andrey Koval <koval.andrey@gmail.com>
2015-07-16 23:00:42 -0700

    #14 numbers are as good

--- reports/extend/standardize_ISR.R
+++ reports/extend/standardize_ISR.R
@@ -10,7 +10,7 @@ library(dplyr)
 ds <- readRDS('./data/shared/ds1.rds')
 
 ## @knitr basic_view
-keepvar <-  c("output_file", "subject_count","pp_TAU_00", "p_SIGMA", "p_GAMMA_00" )
+keepvar <-  c("output_file", "subject_count","pc_TAU_00", "pc_TAU_11","pc_SIGMA", "p_GAMMA_00" )
 ds[1:10, keepvar]
 dim(ds) # rows and columns
 
@@ -19,9 +19,9 @@ ds$pc_CORR_00 <- ds$pc_TAU_00 / (sqrt(ds$pp_TAU_00)*sqrt(ds$cc_TAU_00))
 ds$pc_CORR_11 <-  ds$pc_TAU_11 / (sqrt(ds$pp_TAU_11)*sqrt(ds$cc_TAU_11))
 ds$pc_CORR_residual <-  ds$pc_SIGMA / (sqrt(ds$p_SIGMA) * sqrt(ds$c_SIGMA))
 
-keepvar <- c("study_name","output_file", "pc_TAU_00", "pp_TAU_00", "cc_TAU_00",  "pc_CORR_00")
-ds[1:10, keepvar]
-
+keepvar <- c("study_name","output_file", "pc_TAU_00", "pp_TAU_00", "cc_TAU_00",  "pc_CORR_00", "pc_CORR_11", "pc_CORR_residual")
+d <- ds[1:10, keepvar]
+summary(d)
 ## @knitr dummy
 # ds <- ds[ds$study_name=="satsa",c(
 #   # "subgroup","model_type"
@@ -42,23 +42,44 @@ ds$outcome_count <- ifelse(is_univariate, 1L, 2L)
 
 ## @knitr alpha_limit
 alpha <- 0.05
-limit <- ((1 - (alpha/2)))
+z_alpha <- qnorm(1 - (alpha/2))
 
 ## @knitr FisherZ
-ds$pc_CORR_00_z <- 1/tanh(ds$pc_CORR_00)
-ds$pc_CORR_11_z <- 1/tanh(ds$pc_CORR_11)
-ds$pc_CORR_residual_z <- 1/tanh(ds$pc_CORR_residual)
-# ds[1:10,c("output_file","pc_TAU_00", "pp_TAU_00", "cc_TAU_00",  "pc_CORR_00", "pc_CORR_00_z" )]
+ds$pc_CORR_00_z <- atanh(ds$pc_CORR_00)
+ds$pc_CORR_11_z <- atanh(ds$pc_CORR_11)
+ds$pc_CORR_residual_z <- atanh(ds$pc_CORR_residual)
+ds[1:10,c("output_file","pc_TAU_00", "pp_TAU_00", "cc_TAU_00",  "pc_CORR_00", "pc_CORR_00_z" )]
+
+# # compute z test statistic
+# ds$pc_CORR_00_z_test <- ds$pc_CORR_00_z / sqrt((ds$subject_count - 3))
+# ds$pc_CORR_11_z_test <- ds$pc_CORR_11_z / sqrt((ds$subject_count - 3))
+# ds$pc_CORR_residual_z_test <- ds$pc_CORR_residual_z / sqrt((ds$subject_count - 3))
+# ds[1:10,c("output_file","pc_TAU_00", "pc_TAU_00_pval", "pc_CORR_00", "pc_CORR_00_z", "pc_CORR_00_z_test" )]
+#
 
 
-## @knitr confidence_limits
-ds$pc_ZETA_00_low <- ds$pc_CORR_00 - (limit * sqrt( 1 / (ds$subject_count - 3)))
-ds$pc_ZETA_00_high <- ds$pc_CORR_00 + (limit * sqrt( 1 / (ds$subject_count - 3)))
-ds$pc_ZETA_11_low <- ds$pc_CORR_11 - (limit * sqrt( 1 / (ds$subject_count - 3)))
-ds$pc_ZETA_11_high <- ds$pc_CORR_11 + (limit * sqrt( 1 / (ds$subject_count - 3)))
-ds$pc_ZETA_residual_low <- ds$pc_CORR_residual - (limit * sqrt( 1 / (ds$subject_count - 3)))
-ds$pc_ZETA_residual_high <- ds$pc_CORR_residual + (limit * sqrt( 1 / (ds$subject_count - 3)))
-ds[1:10,c("output_file", "subject_count", "pc_CORR_00", "pc_ZETA_00_low","pc_ZETA_00_high")]
+
+
+
+
+
+
+
+# CI for the z test statistic
+## @knitr confidence_limit
+ds$pc_ZETA_00_low <- ds$pc_CORR_00_z - (z_alpha * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_00_high <- ds$pc_CORR_00_z + (z_alpha * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_11_low <- ds$pc_CORR_11_z - (z_alpha * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_11_high <- ds$pc_CORR_11_z + (z_alpha * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_residual_low <- ds$pc_CORR_residual_z - (z_alpha * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_residual_high <- ds$pc_CORR_residual_z + (z_alpha * sqrt( 1 / (ds$subject_count - 3)))
+# ds$pc_ZETA_00_low <- ds$pc_CORR_00 - (limit * sqrt( 1 / (ds$subject_count - 3)))
+# ds$pc_ZETA_00_high <- ds$pc_CORR_00 + (limit * sqrt( 1 / (ds$subject_count - 3)))
+# ds$pc_ZETA_11_low <- ds$pc_CORR_11 - (limit * sqrt( 1 / (ds$subject_count - 3)))
+# ds$pc_ZETA_11_high <- ds$pc_CORR_11 + (limit * sqrt( 1 / (ds$subject_count - 3)))
+# ds$pc_ZETA_residual_low <- ds$pc_CORR_residual - (limit * sqrt( 1 / (ds$subject_count - 3)))
+# ds$pc_ZETA_residual_high <- ds$pc_CORR_residual + (limit * sqrt( 1 / (ds$subject_count - 3)))
+d <-ds[1:10,c("output_file","pc_TAU_00", "pc_TAU_00_pval", "pc_CORR_00", "pc_CORR_00_z",  "pc_ZETA_00_low","pc_ZETA_00_high")]
 
 
 
@@ -70,10 +91,15 @@ ds$pc_CI95_11_low <- tanh(ds$pc_ZETA_11_low)
 ds$pc_CI95_11_high <-   tanh(ds$pc_ZETA_11_high)
 ds$pc_CI95_residual_low <- tanh(ds$pc_ZETA_residual_low)
 ds$pc_CI95_residual_high <-   tanh(ds$pc_ZETA_residual_high)
-ds[1:10,c("output_file", "pc_ZETA_00_low","pc_ZETA_00_high", "pc_CI95_00_low","pc_CI95_00_high"  )]
-ds[1:20, c("pc_TAU_00", "pc_TAU_00_pval", "pc_CI95_00_low","pc_CORR_00", "pc_CI95_00_high" )]
-
-
+# ds$pc_CI95_00_low <- tanh(ds$pc_ZETA_00_low)
+# ds$pc_CI95_00_high <-  tanh(ds$pc_ZETA_00_high)
+# ds$pc_CI95_11_low <- tanh(ds$pc_ZETA_11_low)
+# ds$pc_CI95_11_high <-   tanh(ds$pc_ZETA_11_high)
+# ds$pc_CI95_residual_low <- tanh(ds$pc_ZETA_residual_low)
+# ds$pc_CI95_residual_high <-   tanh(ds$pc_ZETA_residual_high)
+d <-ds[1:10,c("output_file","pc_TAU_00", "pc_TAU_00_pval", "pc_CORR_00",
+              "pc_CORR_00_z",  "pc_ZETA_00_low","pc_ZETA_00_high", "pc_CI95_00_low", "pc_CI95_00_high"
+              )]
 
 
 ## @knitr dummy
