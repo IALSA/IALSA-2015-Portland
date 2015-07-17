7bdbdf544c98b397859b749281211efea7d31997
Andrey Koval <koval.andrey@gmail.com>
2015-07-16 17:20:22 -0700

    #14 updated equations, still don't work
    @wibeasley, please help me trace the formulas. I cannot understand why
    CIs produce illegal values. IF you look at the BISR graph you'll see
    that some of the correlation should not be significant because that
    don't fall within the CI. I can't spend any more time on this.

--- reports/extend/standardize_ISR.R
+++ reports/extend/standardize_ISR.R
@@ -1,14 +1,28 @@
 rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
 #####################################
 
+## @knitr load_packages
+library(knitr)
+library(testit, quietly=TRUE) #For asserts
+library(dplyr)
+
 ## @knitr load_data
 ds <- readRDS('./data/shared/ds1.rds')
+
+## @knitr basic_view
+keepvar <-  c("output_file", "subject_count","pp_TAU_00", "p_SIGMA", "p_GAMMA_00" )
+ds[1:10, keepvar]
+dim(ds) # rows and columns
 
 ## @knitr standardize_coefficients
 ds$pc_CORR_00 <- ds$pc_TAU_00 / (sqrt(ds$pp_TAU_00)*sqrt(ds$cc_TAU_00))
 ds$pc_CORR_11 <-  ds$pc_TAU_11 / (sqrt(ds$pp_TAU_11)*sqrt(ds$cc_TAU_11))
-ds$pc_CORR_residual <-  ds$pc_SIGMA / ((sqrt(ds$p_SIGMA) * sqrt(ds$c_SIGMA)))
+ds$pc_CORR_residual <-  ds$pc_SIGMA / (sqrt(ds$p_SIGMA) * sqrt(ds$c_SIGMA))
 
+keepvar <- c("study_name","output_file", "pc_TAU_00", "pp_TAU_00", "cc_TAU_00",  "pc_CORR_00")
+ds[1:10, keepvar]
+
+## @knitr dummy
 # ds <- ds[ds$study_name=="satsa",c(
 #   # "subgroup","model_type"
 #   "output_file"
@@ -19,77 +33,79 @@ ds$pc_CORR_residual <-  ds$pc_SIGMA / ((sqrt(ds$p_SIGMA) * sqrt(ds$c_SIGMA)))
 # ds[1:100, ]
 
 
-# ## trim to make more managable
-# keepvar <- c(
-#   #"study_name","model_number", "subgroup", "model_type","physical_construct","physical_measure", "cognitive_construct","cognitive_measure",
-#   # "converged", "output_file",
-#   "pp_TAU_00" ,"cc_TAU_00","pc_CORR_00"
-# )
-# # keepvar <- c("study_name","model_number", "subgroup", "model_type","physical_construct","physical_measure", "cognitive_construct","cognitive_measure", "converged", "output_file", "corr_int", "corr_slope",  "corr_residual",    "ciu_corr_int",    "cil_corr_int",    "ciu_corr_slope",  "cil_corr_slope", "ciu_corr_residual",       "cil_corr_residual", "p_cov_int", "p_cov_slope", "p_cov_res", )
-# # keepvar <- c("model_number","study_name","subgroup", "model_type","physical_construct","cognitive_construct","physical_measure","cognitive_measure", "output_file", "converged")
-#
-# # reduce number of columns
-# ds <- ds[ , keepvar]
-# head(ds)
-# summary(ds)
-
-
-# standardize_coefficients
-# ds$corr_int <- ds$cov_int/ (sqrt(ds$var_int_physical)*sqrt(ds$var_int_cog))
-# ds$corr_slope <- ds$cov_slope/ (sqrt(ds$var_slope_physical)*sqrt(ds$var_slope_cog))
-# ds$corr_residual <- ds$cov_residual/ (sqrt(ds$var_residual_physical)*sqrt(ds$var_residual_cog))
-
+## @knitr bivariate_test
 is_univariate <- grepl(pattern="^u\\d$", x=ds$model_number)
 is_bivariate <- grepl(pattern="^b\\d$", x=ds$model_number)
 testit::assert("The model number should match the univariate or bivariate pattern.", is_univariate | is_bivariate)
 ds$outcome_count <- ifelse(is_univariate, 1L, 2L)
 
+
+## @knitr alpha_limit
 alpha <- 0.05
 limit <- ((1 - (alpha/2)))
 
-# CI for the intercept
-ds$pc_ZETA_00_high <- ds$pc_CORR_00 + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-ds$pc_ZETA_00_low <- ds$pc_CORR_00 - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-ds$pc_CI95_00_high <- tanh(ds$pc_ZETA_00_high)
+## @knitr FisherZ
+ds$pc_CORR_00_z <- 1/tanh(ds$pc_CORR_00)
+ds$pc_CORR_11_z <- 1/tanh(ds$pc_CORR_11)
+ds$pc_CORR_residual_z <- 1/tanh(ds$pc_CORR_residual)
+# ds[1:10,c("output_file","pc_TAU_00", "pp_TAU_00", "cc_TAU_00",  "pc_CORR_00", "pc_CORR_00_z" )]
+
+
+## @knitr confidence_limits
+ds$pc_ZETA_00_low <- ds$pc_CORR_00 - (limit * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_00_high <- ds$pc_CORR_00 + (limit * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_11_low <- ds$pc_CORR_11 - (limit * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_11_high <- ds$pc_CORR_11 + (limit * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_residual_low <- ds$pc_CORR_residual - (limit * sqrt( 1 / (ds$subject_count - 3)))
+ds$pc_ZETA_residual_high <- ds$pc_CORR_residual + (limit * sqrt( 1 / (ds$subject_count - 3)))
+ds[1:10,c("output_file", "subject_count", "pc_CORR_00", "pc_ZETA_00_low","pc_ZETA_00_high")]
+
+
+
+
+## @knitr ci_correlation
 ds$pc_CI95_00_low <- tanh(ds$pc_ZETA_00_low)
-
-# # CI for the intercept
-# ds$int_zetau <- ds$corr_int + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-# ds$int_zetal <- ds$corr_int - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-# ds$ciu_corr_int <- tanh(ds$int_zetau)
-# ds$cil_corr_int <- tanh(ds$int_zetal)
-
-# CI for the slope
-ds$pc_ZETA_11_high <- ds$pc_CORR_11 + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-ds$pc_ZETA_11_low <- ds$pc_CORR_11 - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-ds$pc_CI95_11_high <- tanh(ds$pc_ZETA_11_high)
+ds$pc_CI95_00_high <-  tanh(ds$pc_ZETA_00_high)
 ds$pc_CI95_11_low <- tanh(ds$pc_ZETA_11_low)
-
-# # CI for the slope
-# ds$slope_zetau <- ds$corr_slope + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-# ds$slope_zetal <- ds$corr_slope - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-# ds$ciu_corr_slope <- tanh(ds$slope_zetau)
-# ds$cil_corr_slope <- tanh(ds$slope_zetal)
-
-# CI for the residual
-ds$pc_ZETA_residual_high <- ds$pc_CORR_residual + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-ds$pc_ZETA_residual_low <- ds$pc_CORR_residual - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-ds$pc_CI95_residual_high <- tanh(ds$pc_ZETA_residual_high)
+ds$pc_CI95_11_high <-   tanh(ds$pc_ZETA_11_high)
 ds$pc_CI95_residual_low <- tanh(ds$pc_ZETA_residual_low)
-
-# # CI for the residual
-# ds$residual_zetau <- ds$corr_residual + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-# ds$residual_zetal <- ds$corr_residual - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
-# ds$ciu_corr_residual <- tanh(ds$residual_zetau)
-# ds$cil_corr_residual <- tanh(ds$residual_zetal)
+ds$pc_CI95_residual_high <-   tanh(ds$pc_ZETA_residual_high)
+ds[1:10,c("output_file", "pc_ZETA_00_low","pc_ZETA_00_high", "pc_CI95_00_low","pc_CI95_00_high"  )]
+ds[1:20, c("pc_TAU_00", "pc_TAU_00_pval", "pc_CI95_00_low","pc_CORR_00", "pc_CI95_00_high" )]
 
 
+
+
+## @knitr dummy
+# ## @knitr ci_intercept
+# ds$pc_ZETA_00_high <- ds$pc_CORR_00 + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
+# ds$pc_ZETA_00_low <- ds$pc_CORR_00 - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
+# ds$pc_CI95_00_high <- tanh(ds$pc_ZETA_00_high)
+# ds$pc_CI95_00_low <- tanh(ds$pc_ZETA_00_low)
+#
+#
+#
+# ## @knit ci_slope
+# ds$pc_ZETA_11_high <- ds$pc_CORR_11 + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
+# ds$pc_ZETA_11_low <- ds$pc_CORR_11 - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
+# ds$pc_CI95_11_high <- tanh(ds$pc_ZETA_11_high)
+# ds$pc_CI95_11_low <- tanh(ds$pc_ZETA_11_low)
+#
+# ## @knit ci_residual
+#
+# ds$pc_ZETA_residual_high <- ds$pc_CORR_residual + (limit * sqrt( 1 / (ds$subject_count - 3) ) )
+# ds$pc_ZETA_residual_low <- ds$pc_CORR_residual - (limit * sqrt( 1 / (ds$subject_count - 3) ) )
+# ds$pc_CI95_residual_high <- tanh(ds$pc_ZETA_residual_high)
+# ds$pc_CI95_residual_low <- tanh(ds$pc_ZETA_residual_low)
+
+
+## @knitr dummy
 ## add uni/bi indicator
 # table(ds$model_number)
 ds$uni_bi <- stringr::str_sub(ds$model_number,1,1)
 
-## @knitr export_dataset
 
+## @knitr export_dataset
 saveRDS(ds,"./data/shared/ds2.rds")
 #
 # source("./scripts/make_pretty.r")
