f7e0db73e6717c9d670be42b3fa60a75cfc3ee41
Andrey Koval <koval.andrey@gmail.com>
2015-10-17 18:33:05 -0700

    #87 fixed effect extraction ready
    extraction_functions.R needs editing, it's not elegant.

--- scripts/mplus/collect_physical.R
+++ scripts/mplus/collect_physical.R
@@ -13,7 +13,7 @@ pathStudies <- file.path(pathDir,"studies")
 list.files(pathStudies) # inspect participating studies
 
 # @knitr load_sources
-source("./scripts/mplus/extraction_functions.R")
+
 
 
 ## @knitr setGlobals
@@ -28,7 +28,7 @@ e <- list.files(file.path(pathStudies,"ilse/physical"),full.names=T, recursive=T
 f <- list.files(file.path(pathStudies,"lasa/physical"),full.names=T, recursive=T, pattern="out$")
 # g <- list.files(file.path(pathStudies,"nas/physical"),full.names=T, recursive=T, pattern="out$")
 h <- list.files(file.path(pathStudies,"nuage/physical"),full.names=T, recursive=T, pattern="out$")
-i <- list.files(file.path(pathStudies,"octo/OUTPUT_files/physical"),full.names=T, recursive=T, pattern="out$")
+i <- list.files(file.path(pathStudies,"octo/physical"),full.names=T, recursive=T, pattern="out$")
 j <- list.files(file.path(pathStudies,"radc/outputs/physical"),full.names=T, recursive=T, pattern="out$")
 # k <- list.files(file.path(pathStudies,"satsa/physical"),full.names=T, recursive=T, pattern="out$")
 model_path <- c(a,b, d, e, f, h, i, j)
@@ -52,6 +52,10 @@ out_list_all_plus[["study"]] <- study_name
 names(out_list_all_plus)
 list_object <- out_list_all_plus
 # @knitr
-ds <- collect_model_results(out_list_all_plus)
+source("./scripts/mplus/extraction_functions.R")
+ds <- results
+destination <- "./projects/physical/outputs/physical"
+write.csv(results, paste0(destination,".csv") , row.names=F)
+saveRDS(results, paste0(destination,".rds") )
 
 
