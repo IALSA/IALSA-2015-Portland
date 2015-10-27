cded64855d5862ec754f51cc11b4ca42d5924f70
Will Beasley <wibeasley@hotmail.com>
2015-10-27 15:54:31 -0500

    refactor extract_fixed

--- reports/physical/apa_tables/apa_tables.R
+++ reports/physical/apa_tables/apa_tables.R
@@ -35,20 +35,23 @@ ds_tau_eav <- ds_wide[, c("run_id", tau_variables)] %>%
   )
 
 # @knitr assemble_table --------------------------------------------------------------
-
 # in_study_name          <- "hrs"
 # in_gender              <- "female"
 # in_model_type          <- "aehplus"
 # in_physical_measure    <- "grip"
 # in_cognitive_measure   <- "gait"
+# d <- ds_wide %>%
+#   dplyr::filter(study_name==in_study_name & subgroup==in_gender & model_type==in_model_type & physical_measure==in_physical_measure & cognitive_measure==in_cognitive_measure)
 
-extract_fixed <- function( d, in_study_name,in_gender, in_physical_measure, in_cognitive_measure, in_model_type="aehplus" ) {
-  d <- ds_wide %>%
-    dplyr::filter(study_name==in_study_name & subgroup==in_gender & model_type==in_model_type & physical_measure==in_physical_measure & cognitive_measure==in_cognitive_measure)
+
+extract_fixed_gender <- function( d, in_gender ) {
+  testit::assert("Only two rows should be passed.", nrow(d)==2L)
+  d <- d %>%
+    dplyr::filter(subgroup==in_gender)
   testit::assert("Only one row should be passed.", nrow(d)==1L)
 
   d_fixed <- data.frame(
-    name           = c("int_age",         "int_edu",         "int_height",     "slope_age",        "slope_edu",       "slope_height"),
+    name           = c("int_age",         "int_edu",         "int_height",      "slope_age",       "slope_edu",       "slope_height"),
     est            = c(d$p_GAMMA_01_est , d$p_GAMMA_02_est , d$p_GAMMA_03_est , d$p_GAMMA_11_est , d$p_GAMMA_12_est , d$p_GAMMA_13_est ),
     se             = c(d$p_GAMMA_01_se  , d$p_GAMMA_02_se  , d$p_GAMMA_03_se  , d$p_GAMMA_11_se  , d$p_GAMMA_12_se  , d$p_GAMMA_13_se  ),
     wald           = c(d$p_GAMMA_01_wald, d$p_GAMMA_02_wald, d$p_GAMMA_03_wald, d$p_GAMMA_11_wald, d$p_GAMMA_12_wald, d$p_GAMMA_13_wald),
@@ -59,8 +62,8 @@ extract_fixed <- function( d, in_study_name,in_gender, in_physical_measure, in_c
 
   d_fixed <- d_fixed %>%
     dplyr::mutate(
-      pval_pretty   = ifelse(pval>.999, ".999", sub("^0(.\\d+)$", "\\1", pval)),
-      dense         = sprintf("%+0.3f(%0.3f),p=%s", est, se, pval_pretty)
+      pval_pretty   = ifelse(pval>.999, ".999", sub("^0(.\\d+)$", "\\1", pval)), #Remove leading zero from p-value.
+      dense         = sprintf("%+0.3f(%0.3f),p=%s", est, se, pval_pretty) #Force est & se to have three decimals (eg, .1 turns into .100).
     ) %>%
     dplyr::select(
       -est,
@@ -72,27 +75,29 @@ extract_fixed <- function( d, in_study_name,in_gender, in_physical_measure, in_c
 
   return( d_fixed )
 }
+extract_fixed <- function( d, in_study_name, in_physical_measure, in_cognitive_measure, in_model_type="aehplus" ) {
+  d <- d %>%
+    dplyr::filter(study_name==in_study_name & model_type==in_model_type & physical_measure==in_physical_measure & cognitive_measure==in_cognitive_measure)
+  testit::assert("Only two rows should exist.", nrow(d)==2L)
 
-ds_fixed_male <- extract_fixed(
+  ds_fixed_male <- extract_fixed_gender(d, in_gender = "male") %>%
+    dplyr::rename_("male" = "dense")
+
+  ds_fixed_female <- extract_fixed_gender(d, in_gender = "female") %>%
+    dplyr::rename_("female" = "dense")
+
+  ds_fixed <- ds_fixed_male %>%
+    dplyr::full_join(ds_fixed_female, by="name")
+
+  return( ds_fixed )
+
+}
+
+ds_fixed <-  extract_fixed(
   ds_wide,
   in_study_name          = "hrs",
-  in_gender              = "male",
   in_physical_measure    = "grip",
   in_cognitive_measure   = "gait"
-  ) %>%
-  dplyr::rename_("male" = "dense")
-
-ds_fixed_female <- extract_fixed(
-  ds_wide,
-  in_study_name          = "hrs",
-  in_gender              = "female",
-  in_physical_measure    = "grip",
-  in_cognitive_measure   = "gait"
-  ) %>%
-  dplyr::rename_("female" = "dense")
-
-
-ds_fixed <- ds_fixed_male %>%
-  dplyr::full_join(ds_fixed_female, by="name")
+)
 
 knitr::kable(ds_fixed, align=c("l", "r", "r"))
