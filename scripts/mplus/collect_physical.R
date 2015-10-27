092db41d839328fe694710a0dad4d36dab8ecb8a
Andrey Koval <koval.andrey@gmail.com>
2015-10-27 14:22:37 -0700

    latest run

--- scripts/mplus/collect_physical.R
+++ scripts/mplus/collect_physical.R
@@ -17,47 +17,58 @@ list.files(pathStudies) # inspect participating studies
 
 
 ## @knitr setGlobals
-# studies <- c("eas", "elsa")
-# out_list_all_plus <- list.files(pathStudies, full.names=T, recursive=T, pattern="out$")
 
+
+
+# @knitr list_paths_studies -------------------------------------------------------------------
 eas <- list.files(file.path(pathStudies,"eas/physical"),full.names=T, recursive=T, pattern="out$")
 elsa <- list.files(file.path(pathStudies,"elsa/physical"),full.names=T, recursive=T, pattern="out$")
-# c <- list.files(file.path(pathStudies,"habc/physical"),full.names=T, recursive=T, pattern="out$")
 hrs <- list.files(file.path(pathStudies,"hrs/physical"),full.names=T, recursive=T, pattern="out$")
 ilse <- list.files(file.path(pathStudies,"ilse/physical"),full.names=T, recursive=T, pattern="out$")
 lasa <- list.files(file.path(pathStudies,"lasa/physical"),full.names=T, recursive=T, pattern="out$")
-# g <- list.files(file.path(pathStudies,"nas/physical"),full.names=T, recursive=T, pattern="out$")
 nuage <- list.files(file.path(pathStudies,"nuage/physical"),full.names=T, recursive=T, pattern="out$")
 octo <- list.files(file.path(pathStudies,"octo/physical"),full.names=T, recursive=T, pattern="out$")
 radc <- list.files(file.path(pathStudies,"radc/physical"),full.names=T, recursive=T, pattern="out$")
 satsa <- list.files(file.path(pathStudies,"satsa/physical"),full.names=T, recursive=T, pattern="out$")
-# model_path <- c(a,b, d, e, f, h, i, j)
-model_path <- c(eas,   hrs,    lasa, nuage, octo, radc, satsa)
-# i <- length(out_list_all_plus)
-# i
+# collect a vector with .out file paths
+(model_path_out <- c(eas, elsa, hrs, ilse, lasa, nuage, octo, radc, satsa))
+# replace ".out" by ".gh5" and get a vector with .gh5 file paths
+(model_path_gh5 <-gsub(".out",".gh5", model_path_out) )
 
-# @knitr list_paths_studies -------------------------------------------------------------------
+
+# @knitr list_out_gh5_study_number_subgroup_type_p1_p2
+
+model_list <- list()
+model_list[["path_out"]] <- model_path_out
+model_list[["path_gh5"]] <- model_path_out
 
 study_name <- list()
-for(i in 1:length(model_path)){
-  a <- strsplit(model_path[i], split="/")
-  selector <- a[[1]] %in% c("studies")
-  element_number <- c(1:length(selector))[selector]
-  study_name[i] <- a[[1]][element_number+1]
+for(i in 1:length(model_list[["path_out"]])){
+  # locate a common root for studies: "~GitHub/IALSA-2015-Portland"
+  (a <- strsplit(model_path_out[i], split="/"))
+  (selector <- a[[1]] %in% c("studies"))
+  (element_number <- c(1:length(selector))[selector])
+  (model_list[["study_name"]][[i]] <- a[[1]][element_number+1])
+  # given a study, get and split the 5-element model name
+  (b <- a[[1]][length(selector)])
+  (b <- strsplit(b, split="_|.out")[[1]])
+  (model_list[["model_number"]][[i]] <- b[1])
+  (model_list[["subgroup"]][[i]] <- b[2])
+  (model_list[["model_type"]][[i]] <- b[3])
+  (model_list[["process1"]][[i]] <- b[4])
+  (model_list[["process2"]][[i]] <- b[5])
 }
+names(model_list)
+str(model_list)
+rm(list=setdiff(ls(), "model_list"))
 
-out_list_all_plus <- list()
-out_list_all_plus[["path"]] <- model_path
-out_list_all_plus[["study"]] <- study_name
-names(out_list_all_plus)
-list_object <- out_list_all_plus
-# @knitr
+# @knitr pass_outputs_through_extraction
 source("./scripts/mplus/extraction_functions.R")
 ds <- results
 destination <- "./projects/physical/outputs/physical"
 write.csv(results, paste0(destination,".csv") , row.names=F)
 saveRDS(results, paste0(destination,".rds") )
-saveRDS(out_list_all_plus, "./projects/physical/outputs/out_list.rds")
+saveRDS(model_list, "./projects/physical/outputs/model_list.rds")
 
 
 
