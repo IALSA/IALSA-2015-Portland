ac923dca7deab098eab550d99e2cb9fe71640077
Andrey Koval <koval.andrey@gmail.com>
2015-10-30 13:52:21 -0700

    updated scatters of FS
    @ampiccinin , i'm updated the graphs. Now you can see the raw variance
    of the intercepts and closes (TAUs), as well as the correlation
    estimated from the regression through the estimated factor scores

--- reports/physical/scatter_matrix/scatter_matrix.R
+++ reports/physical/scatter_matrix/scatter_matrix.R
@@ -47,27 +47,29 @@ head(dsL)
 proto_scatter <- function( dsL, xName, yName ) {
   (minx <- min(dsL[,xName],na.rm = T))
   (miny <- min(dsL[,yName],na.rm = T))
+
   m <- lm(as.formula(paste(yName, "~", xName)), dsL)
+
   eqn <- as.character(as.expression( #See Recipe 5.9 in Chang, 2013
-    substitute(italic(y)==a + b * italic(x) * "," ~ ~italic(r)^2 ~ "=" ~ r2,
+    substitute(italic(y)==a + b * italic(x) * "," ~ ~italic(r) ~ "=" ~ r2,
                list(a=format(coef(m)[1], digits=3),#The intercept
                     b=format(coef(m)[2], digits=3), #The slope
-                    r2=format(summary(m)$r.squared, digits=3)))
+                    r2=format(sqrt(summary(m)$r.squared), digits=3)))
   ))
 
 
   g <- ggplot2::ggplot(dsL,aes_string(x=xName, y=yName, fill="BAGE"))+
   geom_point(shape=21,size=5, alpha=.1)+
-  geom_smooth(aes_string(y=yName), method="loess",color="black", size=.45, fill="gray70", alpha=.33, linetype="dashed", na.rm=T)+
-  geom_smooth(aes_string(y=yName), method="lm",color="red", size=.4, se=F, na.rm=T)+
+  geom_smooth(aes_string(y=yName), method="loess",color="black", size=.45, fill="gray70", alpha=.33, linetype="dashed", na.rm=T, span=1.5)+
+  geom_smooth(aes_string(y=yName), method="lm",color="red", size=.4, se=F, na.rm=T, span=1)+
   scale_fill_gradient2(low="#7fbf7b", mid="#f7f7f7", high="#af8dc3", space="Lab")+
   theme(legend.position="none")+
-  annotate("text", size=baseSize-6, label=eqn, x=minx, y=miny, hjust=0, parse=TRUE, color="red")+
+  annotate("text", size=baseSize-6, label=eqn, x=-Inf, y=-Inf, hjust=0, vjust=0, parse=TRUE, color="red")+
   main_theme
 g
 
 }
-# proto_scatter(dsL,xName="s_grip", yName="s_pef")
+proto_scatter(dsL,xName="s_grip", yName="s_pef")
 
 
 #inspect data for one individual
@@ -124,14 +126,19 @@ s2 <- paste0("s_",process2)
 (R_IPIC_display <- paste0("R : ", dsL$R_IPIC_est[1]," (",dsL$R_IPIC_se[1],"), p = ",dsL$R_IPIC_pval[1] ))
 (R_SPSC_display <- paste0("R : ", dsL$R_SPSC_est[1]," (",dsL$R_SPSC_se[1],"), p = ",dsL$R_SPSC_pval[1] ))
 
+(pp_TAU_00_display <- paste0("TAU : ", results[1,"pp_TAU_00_est"]," (",results[1,"pp_TAU_00_se"],"), p = ",results[1,"pp_TAU_00_pval"] ))
+(pp_TAU_11_display <- paste0("TAU : ", results[1,"pp_TAU_11_est"]," (",results[1,"pp_TAU_11_se"],"), p = ",results[1,"pp_TAU_11_pval"] ))
+
 # to do : adapt examples in https://github.com/IALSA/IALSA-2015-Portland/issues/99
 # R_IPIC_display <- sprintf("R_ICIP: %f (%f), p=%0.3f", dsL$R_IPIC_est[1], dsL$R_IPIC_se[1], dsL$R_IPIC_pval[1])
 # substitute(italic(r)(italic(b)0[ip], italic(b)0[ic]), list(ip=dsL$process1[1], cp=dsL$process2[1]))
 
-
+(p1_display <- paste0(R_IPIC_display, " \n ", pp_TAU_00_display))
+(p2_display <- paste0(R_SPSC_display, " \n ", pp_TAU_11_display))
+# browser()
 (a <- proto_scatter(dsL,x=i2, y=i1 ) + theme(legend.position="none") +
-  annotate(geom="text", size=baseSize-6, x=max_i2, y=max_i1, label=R_IPIC_display, hjust=1)+
-  # annotate(geom="text", size=baseSize-8, x=max_i2, y=min_i1, label=paste0(dsL$subgroup[1], "\n", "N = ",sample_N))+
+  # annotate(geom="text", size=baseSize-6, x=max_i2, y=max_i1, label=p1_display, hjust=1)+
+  annotate(geom="text", size=baseSize-6, x=Inf, y=Inf, label=p1_display, hjust=1, vjust=1)+
   geom_vline(x=p2_gamma_00,  color="gray60", size=3, alpha=.15)+
   annotate("text", size=baseSize-6, label="symbol(gamma[0][0])", x=p2_gamma_00, y=min_i1, hjust=.5, vjust=1.5, parse=TRUE, size=7, color="black") +
   geom_hline(y=p1_gamma_00,  color="grey", size=3, alpha=.15)+
@@ -139,8 +146,7 @@ s2 <- paste0("s_",process2)
   labs(x=  process2, y= process1,title= "INTERCEPT \n (estimated factor scores)" ))
 
 (b <- proto_scatter(dsL,x=s2, y=s1)+
-  annotate(geom="text", size=baseSize-6, x=max_s2, y=max_s1, label=R_SPSC_display, hjust=1)+
-  # annotate(geom="text", size=baseSize-8, x=max_s2, y=min_s1, label=paste0(dsL$subgroup[1], "\n", "N = ",sample_N))+
+  annotate(geom="text", size=baseSize-6, x=Inf, y=Inf, label=p2_display, hjust=1, vjust=1)+
   geom_vline(x=p2_gamma_10,  color="gray60", size=3, alpha=.2)+
   annotate("text",  size=baseSize-6, label="symbol(gamma[1][0])", x=p2_gamma_10, y=min_s1, hjust=.5, vjust=1.5, parse=TRUE, size=7, color="black") +
   geom_hline(y=p1_gamma_10,  color="grey", size=3, alpha=.2)+
@@ -172,6 +178,18 @@ grid::grid.newpage()
 
 # sample_size <- 10000 # activate for testing and development
 # (sample_size <- length(unique(dsL$id)))
+#
+# dsL <- get_gh5_data(file=model_list,
+#                     study = "hrs",
+#                     subgroup = "female",
+#                     model_type = "aehplus",
+#                     process1 = "pef",
+#                     process2 = "gait",
+#                     age_center = 75)
+# # dsL <- dsL[dsL$id %in% sample(unique(dsL$id), 100), ]
+# int_slope(dsL) # create scatterplot
+#
+#
 
 
 
