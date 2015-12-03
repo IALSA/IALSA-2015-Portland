fa769fdcc901fceab4704aebd3aa85a2cfb94052
Andrey Koval <koval.andrey@gmail.com>
2015-12-03 13:53:46 -0800

    update domain map

---
+++ reports/model_space/Cog-Domain-Map-2.md
@@ -0,0 +1,112 @@
+# Cog Domain Map - Portland Workshop
+
+
+
+<!--  Set the working directory to the repository's base directory; this assumes the report is nested inside of two directories.-->
+
+
+<!-- Set the report-wide options, and point to the external code file. -->
+
+
+<!-- Load the sources.  Suppress the output when loading sources. --> 
+
+
+<!-- Load 'sourced' R files.  Suppress the output when loading packages. --> 
+
+
+<!-- Load any Global functions and variables declared in the R file.  Suppress the output. --> 
+
+
+<!-- Declare any global functions specific to a Rmd output.  Suppress the output. --> 
+
+
+<!-- Load the datasets.   -->
+
+
+<!-- Tweak the datasets.   -->
+
+
+
+
+```
+List of 1
+ $ strip.text.x:List of 8
+  ..$ family    : NULL
+  ..$ face      : NULL
+  ..$ colour    : chr "black"
+  ..$ size      : num 7
+  ..$ hjust     : NULL
+  ..$ vjust     : NULL
+  ..$ angle     : num 0
+  ..$ lineheight: NULL
+  ..- attr(*, "class")= chr [1:2] "element_text" "element"
+ - attr(*, "class")= chr [1:2] "theme" "gg"
+ - attr(*, "complete")= logi FALSE
+```
+## Prototype
+
+Andrea, you've suggested the followed  structure to investigate replication potential across studies and cognitive domains:
+![prototype](./images/Piccinin_Cognitive_Domains.PNG)  
+
+I like how it simplifies the search for overlaps among measures. I developed the following interpretation of the demensionality in the graph above. 
+
+## Domain Map
+
+
+
+![](dev_cog_domain_map/domain_map-1.png) 
+
+The domain map above is the actual data provided by the model in the Portland modeling collective right now. The colors  represent various cognitive domains, as identified by the participants in the model names. The colors are meant to be categorical, the change in hue is coincidental with alphabetic name.
+
+
+## Cross counts with Physical Measures
+
+We can augment this map with the tile graph of the bivariate model counts: 
+
+
+## Objectives
+
+Now we have a better system for planning our project. Itemizing, we can distinguish the following objectives: 
+
+  1. Reduce the number of colors in the domain map 
+    a. Combine measures into domains  
+    b. remove domains due to few  
+  2. Reduce the number of rows  
+  3. Reduce the number of columns
+  4. Increase the value of numbers
+
+## Combine with other dimensions (Experimental)
+
+
+
+
+## Session Information
+For the sake of documentation and reproducibility, the current report was rendered on a system using the following software.
+
+
+```
+Report rendered by andkov at 2015-12-01, 13:46 -0800
+```
+
+```
+R version 3.2.0 (2015-04-16)
+Platform: x86_64-w64-mingw32/x64 (64-bit)
+Running under: Windows 7 x64 (build 7601) Service Pack 1
+
+locale:
+[1] LC_COLLATE=English_Canada.1252  LC_CTYPE=English_Canada.1252    LC_MONETARY=English_Canada.1252
+[4] LC_NUMERIC=C                    LC_TIME=English_Canada.1252    
+
+attached base packages:
+[1] grid      stats     graphics  grDevices utils     datasets  methods   base     
+
+other attached packages:
+[1] knitr_1.10      lattice_0.20-31 dplyr_0.4.1     ggplot2_1.0.1   shiny_0.12.0   
+
+loaded via a namespace (and not attached):
+ [1] Rcpp_0.11.6      magrittr_1.5     MASS_7.3-40      munsell_0.4.2    colorspace_1.2-6 xtable_1.7-4    
+ [7] R6_2.0.1         stringr_1.0.0    plyr_1.8.2       tools_3.2.0      parallel_3.2.0   gtable_0.1.2    
+[13] DBI_0.3.1        htmltools_0.2.6  yaml_2.1.13      lazyeval_0.1.10  digest_0.6.8     assertthat_0.1  
+[19] formatR_1.2      reshape2_1.4.1   evaluate_0.7     mime_0.3         rmarkdown_0.5.1  stringi_0.4-1   
+[25] scales_0.2.4     httpuv_1.3.2     proto_0.3-10    
+```
---
+++ reports/model_space/cog_domain_map_2.R
@@ -0,0 +1,101 @@
+## @knitr dummy
+############## Developing the tile graph #######################
+
+## @knitr load_source
+
+
+## @knitr load_packages
+library(shiny)
+library(ggplot2)
+library(dplyr)
+library(lattice)
+library(grid)
+
+
+## @knitr load_data
+## if-else conditions for Shiny production
+## "b" in "dsb" is for BASIC
+
+if(basename(getwd())=="dashboard"){
+  dsb <- readRDS('../../data/shared/ds2.rds')
+  source("../../shiny/dashboard/scripts/multiplot_function.R")
+}else{
+  dsb <- readRDS('./data/shared/ds2.rds')
+  source("./shiny/dashboard/scripts/multiplot_function.R")
+}
+
+
+## @knitr tweak_data
+## trim to make more managable
+# keepvar <- c("model_number","study_name","subgroup", "model_type","physical_construct","cognitive_construct","physical_measure","cognitive_measure", "output_file", "converged")
+# ds <- dsb[ , keepvar]
+# dplyr::tbl_df(ds)
+
+
+## @knitr define_themes
+## define common graphical theme for all graphs
+baseSize <- 10
+theme1 <- ggplot2::theme_bw(base_size=baseSize) +
+  ggplot2::theme(title=ggplot2::element_text(colour="gray20",size = baseSize+1)) +
+  ggplot2::theme(axis.text=ggplot2::element_text(colour="gray40", size=baseSize-2)) +
+  ggplot2::theme(axis.title=ggplot2::element_text(colour="gray40")) +
+  ggplot2::theme(panel.border = ggplot2::element_rect(colour="gray80")) +
+  ggplot2::theme(axis.ticks.length = grid::unit(0, "cm")) +
+  ggplot2::theme(text = element_text(size =baseSize+7))
+#   ggplot2::theme(panel.grid.major.x = element_blank())
+#   ggplot2::theme(panel.grid.major.y = element_blank())
+#   ggplot2::theme(panel.grid.minor.x = element_blank())
+#   ggplot2::theme(panel.grid.minor.y = element_blank())
+ggplot2::theme(strip.text.x = element_text(angle = 0, size=baseSize-3, color="black"))
+
+## @knitr declare_globals
+## Define color palette and display labels
+x_name_colors <- c("physical_measure"="#e78ac3",
+                   "study_name"="#8da0cb",
+                   "model_type"="#fc8d62",
+                   "subgroup"="#66c2a5")
+x_name_labels <- c("physical_measure"="Physical Measure",
+                   "study_name"="Study",
+                   "model_type"="Covariates",
+                   "subgroup"="Subgroup")
+
+## @knitr define_graph_functions
+
+
+domain_map <- function(ds, labels){
+  # define the data
+
+  d <- ds %>%
+    dplyr::count_(c("cognitive_measure", "cognitive_construct","study_name"))
+  d$dummy <- factor("dummy")
+  d$cog_meas <- stringr::str_sub(d$cognitive_measure,1,3)
+  d$cog_measure_display <-paste0(stringr::str_sub(d$cognitive_measure,1,6),
+                                 ", ",d$n)
+  # d
+  # str(d)
+  #
+  g <- ggplot2::ggplot(d, aes_string(x="dummy", y="cognitive_measure", label="cog_measure_display", fill="cognitive_construct"))
+  g <- g + geom_tile()
+  g <- g + geom_text(size = baseSize-7, hjust=.4)
+  g <- g + facet_grid(. ~ study_name )
+  # g <- g + coord_flip()
+  g <- g + scale_y_discrete(name = "Cognitive measures", limits=rev(unique(d$cognitive_measure)))
+  g <- g + scale_fill_discrete(name = "Domains")
+  # g <- g + scale_fill_gradient(low="white", high="salmon", na.value = "white")
+  g <- g + labs(title="Studies")
+  g <- g + theme1
+  g <- g + theme(axis.text.y =  element_text(size=baseSize-1),
+                 axis.text.x =  element_blank(),
+                 axis.title.x = element_blank(),
+                 axis.title.y = element_text("Cognitive measure"),
+                 # legend.title = element_blank(),
+                 legend.text =  element_text(),
+                 legend.position="left")
+  return(g)
+}
+
+a <- domain_map(dsb)
+
+# @knitr reproduce ---------------------------------------
+rmarkdown::render(input = "./reports/model_space/Cog-Domain-Map-2.Rmd" ,
+                  output_format="html_document", clean=TRUE)
