8eed4129d573f711a3b3fa31e7892526b969facd
Andrey Koval <koval.andrey@gmail.com>
2016-03-02 10:49:14 -0800

    new templates

--- scripts/templates/reports/R_starter.R
+++ scripts/templates/reports/R_starter.R
@@ -1,33 +1,33 @@
 rm(list=ls(all=TRUE)) #Clear the memory of variables from previous run. This is not called by knitr, because it's above the first chunk.
-cat("\f") # clear console
+
+# ---- load-sources ------------------------------------------------------------
+#Load any source files that contain/define functions, but that don't load any other types of variables
+#   into memory.  Avoid side effects and don't pollute the global environment.
+# source("./SomethingSomething.R")
+
+# ---- load-packages -----------------------------------------------------------
+library(ggplot2) #For graphing
+library(magrittr) #Pipes
+requireNamespace("knitr")
+requireNamespace("scales") #For formating values in graphs
+requireNamespace("dplyr")
+requireNamespace("DT")
+# requireNamespace("RColorBrewer")
+# requireNamespace("plyr")
+# requireNamespace("reshape2") #For converting wide to long
+# requireNamespace("mgcv, quietly=TRUE) #For the Generalized Additive Model that smooths the longitudinal graphs.
+
+# ---- declare-globals ---------------------------------------------------------
+options(show.signif.stars=F) #Turn off the annotations on p-values
+path_input <- "./data-phi-free/derived/motor-trend-car-test.rds"
 
 
 
-# @knitr load_packages
-library(dplyr) # for data manipulation
-library(ggplot2) # for graphing
+# ---- load-data ---------------------------------------------------------------
+ds <- readRDS(path_input) # 'ds' stands for 'datasets'
 
-
-# @knitr load_sources ---------------------------------------
-source("https://raw.githubusercontent.com/andkov/psy532/master/scripts/graphs/main_theme.R")
-
-# @knitr declare_globals ---------------------------------------
-
-# @knitr load_data ---------------------------------------
-
-# @knitr inspect_data ---------------------------------------
-
-# @knitr tweak_data ---------------------------------------
-
-# @kntir basic_table ---------------------------------------
-
-# @knitr basic_graph ---------------------------------------
-
-# @knitr new_chunk ---------------------------------------
-
-
-
-
+# ---- tweak-data --------------------------------------------------------------
+#
 
 
 
