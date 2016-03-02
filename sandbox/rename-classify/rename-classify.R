c098e948bbb9154d71085fae21a2068b51e96ebb
Andrey Koval <koval.andrey@gmail.com>
2016-03-02 13:56:01 -0800

    #119 filtering thru csv maps

---
+++ sandbox/rename-classify/rename-classify.R
@@ -0,0 +1,153 @@
+# knitr::stitch_rmd(script="./sandbox/rename-classify/rename-classify.R", output="./sandbox/rename-classify/rename-classify.md")
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+
+# ---- load_sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+
+# ---- load_packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) #Pipes
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("ggplot2", quietly=TRUE)
+requireNamespace("dplyr", quietly=TRUE) #Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit", quietly=TRUE)
+# requireNamespace("plyr", quietly=TRUE)
+
+# ---- declare_globals ---------------------------------------------------------
+# path_input  <- "./data-phi-free/raw/results-physical-cognitive.csv"
+path_input  <- "./data/shared/results-physical-cognitive.csv"
+path_output <- "./data/shared/results-physical-cognitive.rds"
+figure_path <- './sandbox/rename-classify/stitched_output/'
+
+
+# ---- load_data ---------------------------------------------------------------
+ds0 <- read.csv(path_input, stringsAsFactors=FALSE)
+
+# ---- tweak_data --------------------------------------------------------------
+colnames(ds0)
+
+ds <- ds0 %>% dplyr::arrange_("cognitive_measure")
+
+
+# ---- spell_model_number ------------------------------------------------------
+t <- table(ds$model_number, ds$study_name);t[t==0]<-".";t
+
+
+# ---- spell_subgroup ---------------------------------------------------------
+t <- table(ds$subgroup, ds$study_name);t[t==0]<-".";t
+
+
+
+# ---- spell_model_type
+t <- table(ds$model_type, ds$study_name);t[t==0]<-".";t
+
+
+# ---- correct_model_type ------------------------------------------------------
+# Read in the conversion table, and drop the `notes` variable.
+ds_model_type_key <- read.csv("./sandbox/rename-classify/model-type-entry-table.csv", stringsAsFactors = F) %>%
+  dplyr::select(-notes)
+# Join the model data frame to the conversion data frame.
+ds <- ds %>%
+  dplyr::left_join(ds_model_type_key, by=c("model_type"="entry"))
+
+t <- table(ds$category_short, ds$study_name);t[t==0]<-".";t
+t <- table(ds$model_type, ds$category_short);t[t==0]<-".";t # raw rows, new columns
+
+# Remove the old variable, and rename the cleaned/condensed variable.
+ds <- ds %>%
+  dplyr::select(-model_type) %>% # remove original entries
+  dplyr::rename_("model_type"="category_short") # replace by corrected entries
+
+t <- table(ds$model_type, ds$study_name); t[t==0]<-"."; t
+
+
+# ---- spell_physical_measure -------------------------------------------------
+t <- table(ds$physical_measure, ds$study_name); t[t==0]<-"."; t
+
+# ---- correct_physical_measure ------------------------------------------------
+# Read in the conversion table, and drop the `notes` variable.
+ds_physical_measure_key <- read.csv("./sandbox/rename-classify/physical-measure-entry-table.csv", stringsAsFactors = F) %>%
+  dplyr::select(-notes)
+
+# Join the model data frame to the conversion data frame.
+ds <- ds %>%
+  dplyr::left_join(ds_physical_measure_key, by=c("physical_measure"="entry"))
+
+# review what has been added by the map
+t <- table(ds[ ,"cell_label"], ds[,"study_name"]);t[t==0]<-".";t
+t <- table(ds[ ,"row_label"],  ds[,"study_name"]);t[t==0]<-".";t
+t <- table(ds[ ,"domain"],     ds[,"study_name"]);t[t==0]<-".";t
+
+t <- table(ds[ ,"physical_measure"], ds$cell_label);t[t==0]<-".";t # raw rows, new columns
+
+# Remove the old variable, and rename the cleaned/condensed variable.
+ds <- ds %>%
+  dplyr::select(-physical_measure) %>%
+  dplyr::rename_("physical_measure"="cell_label") %>%
+  dplyr::rename_("physical_measure_pretty"="row_label") %>%
+  dplyr::rename_("phsycial_domain"="domain")
+
+
+t <- table(ds$physical_measure, ds$study_name); t[t==0]<-"."; t
+
+
+
+
+
+## @knitr spell_cognitive_measure
+t <- table(ds$cognitive_measure, ds$study_name);t[t==0]<-".";t
+d <- ds %>% dplyr::group_by_("cognitive_measure","study_name") %>% dplyr::summarize(count=n())
+d <- d %>% dplyr::ungroup() %>% dplyr::arrange_("study_name")
+knitr::kable(d)
+
+# ---- correct_cognitive_measure ------------------------------------------------
+# Read in the conversion table, and drop the `notes` variable.
+ds_cognitive_measure_key <- read.csv("./sandbox/rename-classify/cognitive-measure-entry-table.csv", stringsAsFactors = F) %>%
+  dplyr::select(-notes, -mplus_name, -study_name)
+
+# Join the model data frame to the conversion data frame.
+# ds <- ds %>%
+ds <- ds %>%
+  dplyr::left_join(ds_cognitive_measure_key, by=c("cognitive_measure"="entry"))
+
+t <- table(ds[ ,"cell_label"], ds[,"study_name"]);t[t==0]<-".";t
+t <- table(ds[ ,"row_label"],  ds[,"study_name"]);t[t==0]<-".";t
+t <- table(ds[ ,"domain"],     ds[,"study_name"]);t[t==0]<-".";t
+
+# the table below may be impractical
+t <- table(ds[,"cognitive_measure"], ds[,"cell_label"]);t[t==0]<-".";t # raw rows, new columns
+
+# Remove the old variable, and rename the cleaned/condensed variable.
+ds <- ds %>%
+  dplyr::select(-cognitive_measure) %>%
+  dplyr::rename_("cognitive_measure"="cell_label") %>%
+  dplyr::rename_("cognitive_measure_pretty"="row_label") %>%
+  dplyr::rename_("cognitive_domain"="domain")
+
+
+t <- table(ds$cognitive_measure, ds$study_name); t[t==0]<-"."; t
+
+d <- ds %>% dplyr::filter(is.na(cognitive_measure)) # remove unidentified measures
+# show unidentified measures only
+t <- table(d[ ,"cognitive_measure"], d[ ,"study_name"]); t[t==0]<-"."; t
+
+# ---- test_cog_measures ---------------------------------------
+t <- table(ds$cognitive_measure, ds$study_name);t[t==0]<-".";t
+d <- ds %>% dplyr::group_by_("cognitive_measure","study_name") %>% dplyr::summarize(count=n())
+d <- d %>% dplyr::ungroup() %>% dplyr::arrange_("study_name")
+knitr::kable(d)
+
+# ---- test_cog_domain -----------------------------------------
+t <- table(ds$cognitive_measure, ds$cognitive_domain);t[t==0]<-".";t
+
+
+# ---- export_ready_data ---------------------------------------
+path_output
+saveRDS(ds, path_output)
+
+
+
+
+
