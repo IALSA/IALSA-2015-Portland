51e7a9ed5b8cc6fb187b4b52891ddfe470484928
Andrey Koval <koval.andrey@gmail.com>
2016-09-25 11:07:07 -0700

    #151 inti table assembly

--- scripts/table-assembly.R
+++ scripts/table-assembly.R
@@ -11,63 +11,15 @@ source("./scripts/model-lookup-function.R")
 source("./scripts/functions-table-assembly.R")
 
 
-
-# returns:
-# > names(a)
-# [1] "id"   "coef" "info"
-# unique(catalog$study_name)
-# view_options(catalog, "octo")
-
-# ---- one-study-example-model_type -------
-# a_possibles <- c("fev","fev100","pef") # if there are multiple measure of the contruct
-# a_possibles <- "gait"
-view_options(
-  d = catalog_spread
-  ,study_name_ ="eas"
-  ,full_id = T
-  ,subgroups = c("female")
-  ,model_types = c("aehplus")
-  ,processes_a = "pef"
-  # ,processes_b = "block"
-)
-
-# inspect individual models
-catalog_spread %>%
-  pull_one_model(
-    study_name_ = "eas",
-    subgroup_   = "male",
-    process_a_  = "pef",
-    process_b_  =  "fas",
-    model_type_ = "a"
-  )
-
-# create a baking mix for the pivot : model_type
-baking_mix <- catalog_spread %>%
-  make_baking_mix_model_type(
-    study_name_      = "eas",
-    subgroup_        = "female",
-    model_type_       = c("a","ae","aeh","aehplus"),     # pivot
-    process_a        = "pef",
-    process_b        = "block"
-  )
-lapply(baking_mix, names)
-cake <- bake_the_cake(baking_mix)
-lapply(cake, names) # inspect the cake
-slice <- slice_the_cake(cake)
-knitr::kable(slice)
-
-# create a baking mix for the pivot : process_a
-# baking_mix <- catalog_spread %>%
-#   make_baking_mix_process_a(
-#     study_name_      = "eas",
-#     subgroup_        = "female",
-#     model_type       =  "aehplus",
-#     process_a_to_sum = "pef" # pivot
-#   )
-# lapply(baking_mix, names)
-
-
-
+# ---- table-of-correlations --------------------------
+# PHYS ||            LEVELS            ||            SLOPES            ||         RESIDUALS            ||
+# _____|| a | ae| aeh | aehplus | full || a | ae| aeh | aehplus | full || a | ae| aeh | aehplus | full ||
+# cog_1||                              ||                              ||                              ||
+# cog_2||                              ||                              ||                              ||
+#  ... ||                              ||                              ||                              ||
+# cog_N||______________________________||______________________________||______________________________||
+temp <- catalog_spread; View(temp)
+catalog_spread
 # ---- one-study-example-model_type ----------------------------
 a_possibles <- c("fev","fev100","pef")  # if there are multiple measure of the contruct
 a_possibles <- "gait"
