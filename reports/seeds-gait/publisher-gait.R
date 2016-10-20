cab634c57a1fee9b1c6343e682e770052a0ae283
Andrey Koval <koval.andrey@gmail.com>
2016-10-20 15:37:20 -0400

    #153 kickstarting gait seeds

---
+++ reports/seeds-gait/publisher-gait.R
@@ -0,0 +1,48 @@
+rm(list=ls(all=TRUE))
+########## Production of reports from .Rmd files ###
+
+path_eas   <- base::file.path("./reports/seeds-pulmonary/seed-eas.Rmd")
+path_elsa  <- base::file.path("./reports/seeds-pulmonary/seed-elsa.Rmd")
+path_hrs   <- base::file.path("./reports/seeds-pulmonary/seed-hrs.Rmd")
+path_ilse  <- base::file.path("./reports/seeds-pulmonary/seed-ilse.Rmd")
+path_lasa  <- base::file.path("./reports/seeds-pulmonary/seed-lasa.Rmd")
+path_nuage <- base::file.path("./reports/seeds-pulmonary/seed-nuage.Rmd")
+path_octo  <- base::file.path("./reports/seeds-pulmonary/seed-octo.Rmd")
+path_map   <- base::file.path("./reports/seeds-pulmonary/seed-map.Rmd")
+path_satsa <- base::file.path("./reports/seeds-pulmonary/seed-satsa.Rmd")
+
+#  Define groups of reports
+allReports<- c(
+   path_eas
+  , path_elsa # fails at put_stat_frosting() b/c contains only 'aehplus'
+  ,path_hrs
+  # path_ilse # does not have pulmonary measure
+  ,path_lasa  # fails at put_stat_frosting() b/c contains only 'aehplus'
+  # path_nuage # does not have pulmonary measure
+  ,path_octo
+  ,path_map
+  ,path_satsa
+)
+# Place report paths HERE ###########
+pathFilesToBuild <- c(allReports) ##########
+
+
+testit::assert("The knitr Rmd files should exist.", base::file.exists(pathFilesToBuild))
+# Build the reports
+for( pathFile in pathFilesToBuild ) {
+  #   pathMd <- base::gsub(pattern=".Rmd$", replacement=".md", x=pathRmd)
+  rmarkdown::render(input = pathFile,
+                    output_format=c(
+                      "html_document"
+                       #, "pdf_document"
+                      # ,"md_document"
+                      ,"word_document"
+                    ),
+                    clean=TRUE)
+}
+
+# To generate the static website from the htmls that have been printed in the code above
+# 1) Select the "gh-pages" branch of your project's repository in GitHub client
+# 2) Open command line terminal and change directory to the root folder of your repository that you've cloned onto your hard drive using GitHub client (type "cmd" in the address line of the File Explorer opened on root folder of your repository's clone)
+# 3) type "bundle install" to install Bundler if you're creating the website for the first time
+# 4) type "bundle exec jekyll serve" to build site and serve to localhost:4000
