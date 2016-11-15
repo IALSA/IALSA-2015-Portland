ada3d5fa49df3d79bb41c0103cbee12ebcb7db10
Andrey Koval <koval.andrey@gmail.com>
2016-11-15 10:12:59 -0500

    init script to check for duplicates

--- manipulation/screen-for-duplicates.R
+++ manipulation/screen-for-duplicates.R
@@ -18,13 +18,34 @@ full.names=F, recursive=T, pattern="out$")
 
 
 
-feb27
-jun16
-nov06
-nov14
+# feb27
+# jun16
+# nov06
+# nov14
 
 intersect(feb27, jun16)
 intersect(feb27, nov06)
-intersect(feb27, nov14)
+intersect(feb27, nov14) #
+setdiff(feb27, nov14) # unique to first
+
+file.copy(from, to, overwrite = recursive, recursive = FALSE,
+          copy.mode = TRUE, copy.date = FALSE)
+
+
+intersect(jun16, nov06)
+intersect(jun16, nov14)
 
 intersect(nov06, nov14)
+
+# ---- duplicates-in-catalog -------------------
+catalog <- read.csv("./data/shared/pc-2-parsed-results-computed_ci.csv", header = T,  stringsAsFactors=FALSE)
+
+catalog %>%
+  dplyr::filter(study_name == "satsa") %>%
+  dplyr::group_by_(.dots = model_components$id) %>%
+  dplyr::summarize(n = n()) %>%
+  dplyr::arrange(desc(n))
+  dplyr::filter(model_type == "aehplus") %>%
+  dplyr::filter(process_a == "fev" & process_b == "analogies")
+
+  dplyr::select_(.dots = c(model_components$id,"output_file"))
