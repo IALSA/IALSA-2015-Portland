070a37ffaeca7b31ea8f086b23c37d60a9d13abf
Andrey Koval <koval.andrey@gmail.com>
2017-05-08 11:13:52 -0400

    kickstarting file manipulation script

---
+++ manipulation/file-manipulation.R
@@ -0,0 +1,29 @@
+
+
+# desing a function to rename many files
+folder <- "./studies/eas/physical/"
+
+add_extension <- function(folder, ext, fire=FALSE){
+  # values for testing
+  # folder <- "./studies/eas/physical/"
+  # ext <- ".dat"
+  #
+  names_full <- list.files(folder,recursive = T, full.names = T)
+  names_base <- basename(names_full)
+  with_extension <- grep("[.]\\w+$", names_base, value = T)
+  without_extension <- setdiff(names_base, with_extension)
+  num_key  <- names_base %in% without_extension
+  #
+  path_old <- names_full[num_key]
+  path_new <- paste0(path_old,ext)
+  #
+  # print(without_extension)
+  #
+  if(fire){
+    file.rename(from = path_old, to = path_new)
+  }
+
+}
+folder <- "./studies/eas/physical/"
+add_extension(folder,".dat", fire=F)
+add_extension(folder,".dat", fire=T)
