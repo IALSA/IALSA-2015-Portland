4a65d14d9e97b8293b408695776db4dad6a22d40
Andrey Koval <koval.andrey@gmail.com>
2017-07-06 13:15:29 -0400

    broke down into three groups

--- sandbox/meta-analysis/meta-analysis-demo.R
+++ sandbox/meta-analysis/meta-analysis-demo.R
@@ -34,19 +34,19 @@ requireNamespace("DT", quietly=TRUE) # for dynamic tables
 # ---- load-data -------------------------------------------------------------
 variable_names <- c("study_name","process_a", "process_b", "subgroup", "n", "est", "se")
 proto <- list(
-  row01 = c("OCTO"  ,"PEF"  ,"Clock" ,"Men"   ,138  ,	0.27	, 0.14),
-  row02 = c("OCTO"  ,"PEF"  ,"Clock" ,"Women" ,275  ,	0.24	, 0.11),
-  row03 = c("MAP"   ,"FEV1" ,"Ideas" ,"Men"   ,321	,-0.02	, 0.08),
-  row04 = c("MAP"   ,"FEV1" ,"Ideas" ,"Women" ,935	, 0.19	, 0.06),
-  row05 = c("EAS"   ,"PEF"  ,"MMSE"  ,"Men"   ,324	, 0.31	, 0.16),
-  row06 = c("EAS"   ,"PEF"  ,"MMSE"  ,"Women" ,545	, 0.18	, 0.1	),
-  row07 = c("MAP"   ,"FEV1" ,"MMSE"  ,"Men"   ,321	, 0.04	, 0.08),
-  row08 = c("MAP"   ,"FEV1" ,"MMSE"  ,"Women" ,935	, 0.02	, 0.06),
-  row09 = c("NAS"   ,"FEV1" ,"MMSE"  ,"Men"   ,1131 , 0.20	, 0.07),
-  row10 = c("OCTO"  ,"PEF"  ,"MMSE"  ,"Men"   ,140	, 0.66	, 0.14),
-  row11 = c("OCTO"  ,"PEF"  ,"MMSE"  ,"Women" ,276	, 0.10  , 0.13),
-  row12 = c("SATSA" ,"FEV1" ,"MMSE"  ,"Men"   ,299	, 0.16	, 0.12),
-  row13 = c("SATSA" ,"FEV1" ,"MMSE"  ,"Women" ,411	,-0.07	, 0.18)
+  row01 = c("OCTO"  ,"PEF"  ,"Clock" ,"Men"   ,138  ,	0.27 ,0.14),
+  row02 = c("OCTO"  ,"PEF"  ,"Clock" ,"Women" ,275  ,	0.24 ,0.11),
+  row03 = c("MAP"   ,"FEV1" ,"Ideas" ,"Men"   ,321  ,-0.02 ,0.08),
+  row04 = c("MAP"   ,"FEV1" ,"Ideas" ,"Women" ,935  , 0.19 ,0.06),
+  row05 = c("EAS"   ,"PEF"  ,"MMSE"  ,"Men"   ,324  , 0.31 ,0.16),
+  row06 = c("EAS"   ,"PEF"  ,"MMSE"  ,"Women" ,545  , 0.18 ,0.10),
+  row07 = c("MAP"   ,"FEV1" ,"MMSE"  ,"Men"   ,321  , 0.04 ,0.08),
+  row08 = c("MAP"   ,"FEV1" ,"MMSE"  ,"Women" ,935  , 0.02 ,0.06),
+  row09 = c("NAS"   ,"FEV1" ,"MMSE"  ,"Men"   ,1131 , 0.20 ,0.07),
+  row10 = c("OCTO"  ,"PEF"  ,"MMSE"  ,"Men"   ,140  , 0.66 ,0.14),
+  row11 = c("OCTO"  ,"PEF"  ,"MMSE"  ,"Women" ,276  , 0.10 ,0.13),
+  row12 = c("SATSA" ,"FEV1" ,"MMSE"  ,"Men"   ,299  , 0.16 ,0.12),
+  row13 = c("SATSA" ,"FEV1" ,"MMSE"  ,"Women" ,411  ,-0.07 ,0.18)
 ) %>%
   dplyr::bind_rows() %>%
   t() %>%
@@ -58,6 +58,7 @@ proto[,c("n","est","se")] <- sapply(proto[,c("n","est","se")], as.numeric)
 proto
 # ---- tweak-data --------------------------------------------------------------
 ds_meta <- proto %>%
+  # compute  lower and upper limit of the 95% confidence (green section)
   dplyr::mutate(
     w  = log( (1 + est)/(1 - est) )/2,                    #
     u  = (w * se)/ est,                                   #
@@ -68,37 +69,53 @@ ds_meta <- proto %>%
     x  = est - ( (exp(2*z)-1)  / (exp(2*z)+1)  )      ,   #
     lo = -(x - est),                                      #  low 95% CI
     hi = y + est,                                         #  high 95% CI
-    af = sprintf("%.2f(%.2f,%.2f)", est, lo, hi),         #  estimate(low, high)
-    ac = abs( w/(u^2) ),                                  #
-    ae = u^-2,                                            #
-    ah = est / se,                                        #  wald
-    ai  = (z / 1.96)^2*ae ,                               #
-    ac_sum = sum(ac),                                     #
-    ae_sum = sum(ae)                                      #
+    af = sprintf("%.2f(%.2f,%.2f)", est, lo, hi)          #  estimate(low, high)
+  ) %>%
+  # compute node sizes (red section)
+  dplyr::mutate(
+    ac     = abs( w/(u^2) ),                              #
+    ae     = u^-2                                         # ae and ag are used interchangeably in spreadsheet
+) %>%
+  dplyr::group_by(subgroup) %>%
+  dplyr::mutate(
+    group_average   = sum(ac)/sum(ae)                     # ad
+  ) %>%
+  dplyr::ungroup() %>%
+  dplyr::mutate(
+    overall_average = sum(ac)/sum(ae),                    # ad
+    node_size       = ae / sum(ae) * 100                  # aj
+  ) %>%
+  # enable/disable the next line to focus on a subset of variables
+  # dplyr::select_(.dots=c(variable_names,"ac","ae","group_average","overall_average","node_size")) %>%
+  # compute chi-square statistics (blue section)
+  dplyr::mutate(
+    q = (z / 1.96)^2*ae                                   # ai
   ) %>%
   dplyr::group_by(subgroup) %>%
   dplyr::mutate(
-    ac_sum_group = sum(ac),
-    ae_sum_group = sum(ae)
- ) %>%
+    q_group_sum     = sum(q),
+    q_group_count   = length(!is.na(q))
+  ) %>%
   dplyr::ungroup() %>%
   dplyr::mutate(
-    aj        = ae / ae_sum * 100,                               #
-    node_size = aj
+    q_overall_sum   = sum(q),
+    q_overall_count = length(!is.na(q)),
+    q_group_pval    = pchisq(q_group_sum,   df = q_group_count   - 1, lower.tail = FALSE),
+    q_overall_pval  = pchisq(q_overall_sum, df = q_overall_count - 1, lower.tail = FALSE)
   ) %>%
-  dplyr::select( # suppress during printing:
-    # -w,-u,-ab,-aa,-z,-y,-x,-lo,-hi, -aj
-  ) %>%
+  # enable/disable the next line to focus on a subset of variables
+  # dplyr::select_(.dots=c(variable_names,"q","q_group_sum", "q_group_count","q_group_pval","q_overall_sum","q_overall_count","q_overall_pval")) %>%
   print()
+
 # ---- basic-table --------------------------------------------------------------
 ds <- ds_meta %>%
   dplyr::rename(
     dense = af,
     low95 = lo,
-    high95= hi,
-
+    high95= hi
   ) %>%
-  dplyr::select(c(variable_names,""))
+  dplyr::select_(.dots = c(variable_names,"dense"))
+ds
 # ---- basic-graph --------------------------------------------------------------
 
 # Sonata form report structure
